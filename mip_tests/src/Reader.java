9855548251b6f2c3b6b07a36f53d9c41dc4f3a53
tbohne <tbohne@uos.de>
2018-09-27 12:26:36 +0200

    implemented basic reader for SLP instances

---
+++ mip_tests/src/Reader.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader {
+
+    private static int[][] readMatrix(BufferedReader reader, int numberOfItems) {
+
+        int[][] matrix = new int[numberOfItems][];
+
+        try {
+            String line = reader.readLine().trim();
+
+            int idx = 0;
+
+            while (!line.equals("")) {
+
+                String[] integerStrings = line.split(" ");
+                matrix[idx] = new int[integerStrings.length];
+
+                for (int i = 0; i < integerStrings.length; i++) {
+                    matrix[idx][i] = Integer.parseInt(integerStrings[i]);
+                }
+
+                line = reader.readLine();
+                line = line == null ? "" : line.trim();
+                idx++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return matrix;
+    }
+
+    public static Instance readInstance(String filename) {
+
+        int numberOfItems = 0;
+        int numberOfStacks = 0;
+        int stackCapacity = 0;
+        int[][] stackingConstraints = new int[numberOfItems][];
+        int[][] costs = new int [numberOfItems][];
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            numberOfItems = Integer.parseInt(reader.readLine().trim());
+            numberOfStacks = Integer.parseInt(reader.readLine().trim());
+            stackCapacity = Integer.parseInt(reader.readLine().trim());
+
+            if (reader.readLine().trim().equals("")) {
+                stackingConstraints = readMatrix(reader, numberOfItems);
+            }
+
+            costs = readMatrix(reader, numberOfItems);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Instance(numberOfItems, numberOfStacks, stackCapacity, stackingConstraints, costs);
+    }
+}
