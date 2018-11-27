4c54d2b6cb55abd2649cfa210d9e83230af250fe
tbohne <tbohne@uos.de>
2018-11-27 13:52:31 +0100

    added .csv writer to solution writer

--- mip_tests/src/SolutionWriter.java
+++ mip_tests/src/SolutionWriter.java
@@ -13,6 +13,50 @@ public class SolutionWriter {
         }
     }
 
+    public static String getNameOfMipFormulationCSV(MIPFormulationComparator.Formulation mipFormulation) {
+        switch (mipFormulation) {
+            case BINPACKING:
+                return "BinP";
+            case THREEINDEX:
+                return "3Idx";
+            default:
+                return "";
+        }
+    }
+
+    public static void writeSolutionAsCSV(String filename, Solution sol, MIPFormulationComparator.Formulation mipFormulation) {
+        try {
+
+            File file = new File(filename);
+
+            boolean newFile = false;
+
+            if (!file.exists()) {
+                file.createNewFile();
+                newFile = true;
+            }
+
+            FileWriter fw = new FileWriter(file, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            if (newFile) {
+                bw.write("instance,mip,time,val\n");
+            }
+
+            String mip = getNameOfMipFormulationCSV(mipFormulation);
+            if (!sol.isEmpty()) {
+                bw.write(sol.getNameOfSolvedInstance().replace("instances/slp_instance_", "") + "," + mip + "," + sol.getTimeToSolve() + "," + sol.getObjectiveValue() + "\n");
+            }
+            bw.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void writeSolution(String filename, Solution sol, MIPFormulationComparator.Formulation mipFormulation) {
 
         try {
