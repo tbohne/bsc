df80b96ecb3e45d238f297339d59b882578c442e
tbohne <tbohne@uos.de>
2018-12-21 11:44:07 +0100

    added util class for heuristics / implemented first static method

---
+++ StorageLoadingProblems/src/SLP/constructive_heuristics/HeuristicUtil.java
@@ -0,0 +1,28 @@
+package SLP.constructive_heuristics;
+
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultUndirectedGraph;
+
+public class HeuristicUtil {
+
+    public static void generateStackingConstraintGraph(
+            DefaultUndirectedGraph<String, DefaultEdge> graph,
+            int[] items,
+            int[][] stackingConstraints
+    ) {
+
+        for (int item : items) {
+            graph.addVertex("v" + item);
+        }
+        // For all incoming items i and j, there is an edge if s_ij + s_ji >= 1.
+        for (int i = 0; i < stackingConstraints.length; i++) {
+            for (int j = 0; j < stackingConstraints[0].length; j++) {
+                if (i != j && stackingConstraints[i][j] == 1 ||stackingConstraints[j][i] == 1) {
+                    if (!graph.containsEdge("v" + j, "v" + i)) {
+                        graph.addEdge("v" + i, "v" + j);
+                    }
+                }
+            }
+        }
+    }
+}
