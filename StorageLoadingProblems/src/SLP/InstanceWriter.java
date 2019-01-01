766217b89dbddc4f365a6759429a73a4a6414cba
tbohne <tbohne@uos.de>
2019-01-01 17:33:08 +0100

    adjusted instance set config file to be csv

--- StorageLoadingProblems/src/SLP/InstanceWriter.java
+++ StorageLoadingProblems/src/SLP/InstanceWriter.java
@@ -65,15 +65,10 @@ public class InstanceWriter {
             FileOutputStream fos = new FileOutputStream(file);
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 
-            bw.write("---------- INSTANCE CONFIGURATION ----------\n");
-
-            bw.write("NUMBER_OF_INSTANCES: " + numOfInstances + "\n");
-            bw.write("NUMBER_OF_ITEMS: " + numOfItems + "\n");
-            bw.write("STACK_CAPACITY: " + stackCap + "\n");
-            bw.write("ADDITIONAL_STACK_PERCENTAGE: " + additionalStackPercentage + "\n");
-            bw.write("CHANCE_FOR_ONE_IN_STACKING_CONSTRAINTS: " + chanceForOneInStackingConstraints + "\n");
-            bw.write("COSTS_INCLUSIVE_LOWER_BOUND: " + costsInclusiveLowerBound + "\n");
-            bw.write("COSTS_EXCLUSIVE_UPPER_BOUND: " + costsExclusiveUpperBound + "\n");
+            bw.write("numOfInstances,numOfItems,stackCapacity,additionalStackPercentage,chanceForOneInStackingConstraints,"
+                    + "costsInclusiveLB,costsExclusiveUB\n");
+            bw.write(numOfInstances + "," + numOfItems + "," + stackCap + "," + additionalStackPercentage + ","
+                + chanceForOneInStackingConstraints + "," + costsInclusiveLowerBound + "," + costsExclusiveUpperBound);
 
             bw.close();
 
