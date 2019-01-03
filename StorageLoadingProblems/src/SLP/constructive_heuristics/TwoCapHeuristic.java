c241f9eacf96ccb7b81e4e12350e5f9368ae32ca
tbohne <tbohne@uos.de>
2019-01-03 17:00:02 +0100

    implemented first part of 2cap heuristic (computing completely filled stacks)

--- StorageLoadingProblems/src/SLP/constructive_heuristics/TwoCapHeuristic.java
+++ StorageLoadingProblems/src/SLP/constructive_heuristics/TwoCapHeuristic.java
@@ -1,10 +1,13 @@
 package SLP.constructive_heuristics;
 
 import SLP.Instance;
+import SLP.MCMEdge;
 import SLP.Solution;
 import org.jgrapht.alg.matching.EdmondsMaximumCardinalityMatching;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultUndirectedGraph;
+
+import java.util.ArrayList;
 
 public class TwoCapHeuristic {
 
@@ -15,10 +18,48 @@ public class TwoCapHeuristic {
         this.instance = instance;
     }
 
+    public EdmondsMaximumCardinalityMatching getMatchingBetweenItemPairsAndStacks(ArrayList<MCMEdge> itemPairs) {
+
+        DefaultUndirectedGraph<String, DefaultEdge> graph = new DefaultUndirectedGraph<>(DefaultEdge.class);
+
+        for (MCMEdge e : itemPairs) {
+            graph.addVertex("edge" + e);
+        }
+
+        for (int stack = 0; stack < this.instance.getStacks().length; stack++) {
+            graph.addVertex("stack" + stack);
+        }
+
+        for (int i = 0; i < itemPairs.size(); i++) {
+            for (int j = 0; j < this.instance.getStacks().length; j++) {
+                if (this.instance.getStackConstraints()[itemPairs.get(i).getVertexOne()][j] == 1
+                        && this.instance.getStackConstraints()[itemPairs.get(i).getVertexTwo()][j] == 1) {
+
+                            if (!graph.containsEdge("stack" + j, "edge" + itemPairs.get(i))
+                                    && !graph.containsEdge("edge" + itemPairs.get(i), "stack" + j)) {
+                                        graph.addEdge("edge" + itemPairs.get(i), "stack" + j);
+                            }
+
+                }
+            }
+        }
+
+        EdmondsMaximumCardinalityMatching<String, DefaultEdge> finalMCM = new EdmondsMaximumCardinalityMatching<>(graph);
+        System.out.println("items assigned: " +  finalMCM.getMatching().getEdges().size() * 2);
+        System.out.println("free stacks: " + (this.instance.getStacks().length - finalMCM.getMatching().getEdges().size()));
+
+        System.out.println(finalMCM.getMatching().getEdges());
+
+        return finalMCM;
+    }
+
     public Solution firstApproach(EdmondsMaximumCardinalityMatching mcm, boolean optimizeSolution) {
 
-        return new Solution();
+        ArrayList<MCMEdge> itemPairs = new ArrayList<>();
+        HeuristicUtil.parseItemPairMCM(itemPairs, mcm);
+        this.getMatchingBetweenItemPairsAndStacks(itemPairs);
 
+        return new Solution();
     }
 
     /**
