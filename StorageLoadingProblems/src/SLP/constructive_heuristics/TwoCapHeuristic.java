9d7908a92907612dbb7831b7b3cce12447d0baab
tbohne <tbohne@uos.de>
2019-01-03 17:38:34 +0100

    implemented assignment of items to stacks for 2cap

--- StorageLoadingProblems/src/SLP/constructive_heuristics/TwoCapHeuristic.java
+++ StorageLoadingProblems/src/SLP/constructive_heuristics/TwoCapHeuristic.java
@@ -52,16 +52,22 @@ public class TwoCapHeuristic {
         return finalMCM;
     }
 
-    public HashMap parseItemPairStackCombination(Object edge) {
-        int firstItem = Integer.parseInt(edge.toString().replace("(edge(", "").split(",")[0].trim());
-        int secondItem = Integer.parseInt(edge.toString().replace("(edge(", "").split(",")[1].split(":")[0].replace(")", "").trim());
-        int stack = Integer.parseInt(edge.toString().replace("(edge(", "").split(",")[1].split(":")[1].replace("stack", "").replace(")", "").trim());
+    public HashMap parseItemPairStackCombination(EdmondsMaximumCardinalityMatching mcm) {
 
         HashMap<Integer, ArrayList<Integer>> itemPairStackCombination = new HashMap<>();
-        ArrayList<Integer> items = new ArrayList<>();
-        items.add(firstItem);
-        items.add(secondItem);
-        itemPairStackCombination.put(stack, items);
+
+        for (Object edge : mcm.getMatching().getEdges()) {
+
+            int firstItem = Integer.parseInt(edge.toString().replace("(edge(", "").split(",")[0].trim());
+            int secondItem = Integer.parseInt(edge.toString().replace("(edge(", "").split(",")[1].split(":")[0].replace(")", "").trim());
+            int stack = Integer.parseInt(edge.toString().replace("(edge(", "").split(",")[1].split(":")[1].replace("stack", "").replace(")", "").trim());
+
+            ArrayList<Integer> items = new ArrayList<>();
+            items.add(firstItem);
+            items.add(secondItem);
+            itemPairStackCombination.put(stack, items);
+
+        }
 
         return itemPairStackCombination;
     }
@@ -72,9 +78,38 @@ public class TwoCapHeuristic {
         HeuristicUtil.parseItemPairMCM(itemPairs, mcm);
         EdmondsMaximumCardinalityMatching finalMCM = this.getMatchingBetweenItemPairsAndStacks(itemPairs);
 
-        for (Object edge : finalMCM.getMatching().getEdges()) {
-            HashMap itemPairStackCombination = this.parseItemPairStackCombination(edge);
-            System.out.println(itemPairStackCombination);
+        HashMap itemPairStackCombinations = this.parseItemPairStackCombination(finalMCM);
+
+        for (Object key : itemPairStackCombinations.keySet()) {
+            this.instance.getStacks()[(int)key][0] = ((ArrayList<Integer>)itemPairStackCombinations.get(key)).get(0);
+            this.instance.getStacks()[(int)key][1] = ((ArrayList<Integer>)itemPairStackCombinations.get(key)).get(1);
+        }
+
+        ArrayList<Integer> unmatchedItems = new ArrayList<>();
+        ArrayList<Integer> matchedItems = new ArrayList<>();
+
+        for (int[] stack : this.instance.getStacks()) {
+            for (int item : stack) {
+                if (item != -1) {
+                    matchedItems.add(item);
+                }
+            }
+        }
+
+        for (int item : this.instance.getItems()) {
+            if (!matchedItems.contains(item)) {
+                unmatchedItems.add(item);
+            }
+        }
+
+        // TODO: take costs into account
+        for (int item : unmatchedItems) {
+            for (int[] stack : this.instance.getStacks()) {
+                if (stack[0] == -1) {
+                    stack[0] = item;
+                    break;
+                }
+            }
         }
 
         Solution sol = new Solution(0, false, this.instance);
