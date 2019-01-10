f599add43aaf5bcfeec4ec9bbb7c47999ab2494e
tbohne <tbohne@uos.de>
2019-01-10 22:28:02 +0100

    extended costs script with avg costs

--- StorageLoadingProblems/src/SLP/r_scripts/2cap_scripts/solver_instance_cost.R
+++ StorageLoadingProblems/src/SLP/r_scripts/2cap_scripts/solver_instance_cost.R
@@ -2,6 +2,16 @@ library(ggplot2)
 
 input <- read.csv(file = "../../../../res/solutions/solutions.csv", header = TRUE, sep = ",")
 solverEntries <- subset(input, solver == "BinP" | solver == "3Idx" | solver == "2Cap")
-plotPointsPre <- ggplot(data = solverEntries, aes(x = val, y = instance, color = solver, group = solver)) + geom_point() + xlab("costs") + ylab("instance")
+plotPointsPre <- ggplot(data = solverEntries, aes(x = val, y = instance, color = solver, group = solver)) + geom_point() + xlab("costs") + ylab("instances")
 
-ggsave(plotPointsPre, file = "solver_instance_cost.png", width = 8, height = 8)
+##############################################################################
+binpData <- subset(input, solver == "BinP")
+binpCosts <- subset(binpData, select = c(val))
+paste("avg costs of BinP: ", mean(binpCosts[["val"]]))
+
+threeidxData <- subset(input, solver == "3Idx")
+threeidxCosts <- subset(threeidxData, select = c(val))
+paste("avg costs of 3Idx: ", mean(threeidxCosts[["val"]]))
+##############################################################################
+
+ggsave(plotPointsPre, file = "solver_instance_cost.png", width = 6, height = 4)
