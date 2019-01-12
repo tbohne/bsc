4372223888511501b57da2c943a25f8cd8fe52b9
tbohne <tbohne@uos.de>
2019-01-12 23:42:31 +0100

    implemented basic item representation

---
+++ StorageLoadingProblems/src/SLP/representations/Item.java
@@ -0,0 +1,26 @@
+package SLP.representations;
+
+public class Item {
+
+    private int idx;
+    private int length;
+    private int width;
+
+    public Item(int idx, int length, int width) {
+        this.idx = idx;
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getIdx() {
+        return this.idx;
+    }
+
+    public int getLength() {
+        return  this.length;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+}
