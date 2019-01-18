a9fbaa32a5d8057b85f575fe285cc26c97e45a04
tbohne <tbohne@uos.de>
2019-01-18 10:12:33 +0100

    extended r scripts to cover new solver

--- StorageLoadingProblems/src/SLP/r_scripts/3cap_scripts/solver_instance_cost.R
+++ StorageLoadingProblems/src/SLP/r_scripts/3cap_scripts/solver_instance_cost.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 
 input <- read.csv(file = "../../../../res/solutions/solutions.csv", header = TRUE, sep = ",")
-solverEntries <- subset(input, solver == "BinP" | solver == "3Idx" | solver == "3CapRec" | solver == "3CapPerm")
+solverEntries <- subset(input, solver == "BinP" | solver == "3Idx" | solver == "3Cap" | solver == "3CapRec" | solver == "3CapPerm")
 plotPointsPre <- ggplot(data = solverEntries, aes(x = val, y = instance, color = solver, group = solver)) + geom_point() + xlab("costs") + ylab("instance")
 
 ##############################################################################
@@ -12,6 +12,10 @@ paste("avg costs of BinP: ", mean(binpCosts[["val"]]))
 threeidxData <- subset(input, solver == "3Idx")
 threeidxCosts <- subset(threeidxData, select = c(val))
 paste("avg costs of 3Idx: ", mean(threeidxCosts[["val"]]))
+
+threeCapData <- subset(input, solver == "3Cap")
+threeCapCosts <- subset(threeCapData, select = c(val))
+paste("avg costs of 3Cap: ", mean(threeCapCosts[["val"]]))
 
 threeCapPermData <- subset(input, solver == "3CapPerm")
 threeCapPermCosts <- subset(threeCapPermData, select = c(val))
--- StorageLoadingProblems/src/SLP/r_scripts/3cap_scripts/solver_instance_time.R
+++ StorageLoadingProblems/src/SLP/r_scripts/3cap_scripts/solver_instance_time.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 
 input <- read.csv(file = "../../../../res/solutions/solutions.csv", header = TRUE, sep = ",")
-solverEntries <- subset(input, solver == "BinP" | solver == "3Idx" | solver == "3CapPerm" | solver == "3CapRec")
+solverEntries <- subset(input, solver == "BinP" | solver == "3Idx" | solver == "3Cap" | solver == "3CapPerm" | solver == "3CapRec")
 plotPointsPre <- ggplot(data = solverEntries, aes(x = time, y = instance, color = solver, group = solver)) + geom_point() + xlab("time (s)") + ylab("instance")
 
 ##############################################################################
@@ -12,6 +12,10 @@ paste("avg runtime of BinP: ", mean(binpRuntime[["time"]]))
 threeidxData <- subset(input, solver == "3Idx")
 threeidxRuntime <- subset(threeidxData, select = c(time))
 paste("avg runtime of 3Idx: ", mean(threeidxRuntime[["time"]]))
+
+threeCapData <- subset(input, solver == "3Cap")
+threeCapRuntime <- subset(threeCapData, select = c(time))
+paste("avg runtime of 3Cap: ", mean(threeCapRuntime[["time"]]))
 
 threeCapPermData <- subset(input, solver == "3CapPerm")
 threeCapPermRuntime <- subset(threeCapPermData, select = c(time))
