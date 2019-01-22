6592943561a31825e4dd2bb999b22b7b1e42cd5f
tbohne <tbohne@uos.de>
2019-01-22 16:34:53 +0100

    renamed script dir

--- StorageLoadingProblems/src/SLP/scripts/3cap_scripts/solver_instance_cost.R
+++ StorageLoadingProblems/src/SLP/scripts/3cap_scripts/solver_instance_cost.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 
 input <- read.csv(file = "../../../../res/solutions/solutions.csv", header = TRUE, sep = ",")
-solverEntries <- subset(input, solver == "BinP" | solver == "3Idx" | solver == "3Cap" | solver == "3CapRec" | solver == "3CapPerm")
+solverEntries <- subset(input, solver == "BinP" | solver == "3Idx" | solver == "3Cap")
 plotPointsPre <- ggplot(data = solverEntries, aes(x = val, y = instance, color = solver, group = solver)) + geom_point() + xlab("costs") + ylab("instance")
 
 ##############################################################################
@@ -17,13 +17,13 @@ threeCapData <- subset(input, solver == "3Cap")
 threeCapCosts <- subset(threeCapData, select = c(val))
 paste("avg costs of 3Cap: ", mean(threeCapCosts[["val"]]))
 
-threeCapPermData <- subset(input, solver == "3CapPerm")
-threeCapPermCosts <- subset(threeCapPermData, select = c(val))
-paste("avg costs of 3CapPerm: ", mean(threeCapPermCosts[["val"]]))
+# threeCapPermData <- subset(input, solver == "3CapPerm")
+# threeCapPermCosts <- subset(threeCapPermData, select = c(val))
+# paste("avg costs of 3CapPerm: ", mean(threeCapPermCosts[["val"]]))
 
-threeCapRecData <- subset(input, solver == "3CapRec")
-threeCapRecCosts <- subset(threeCapRecData, select = c(val))
-paste("avg costs of 3CapRec: ", mean(threeCapRecCosts[["val"]]))
+# threeCapRecData <- subset(input, solver == "3CapRec")
+# threeCapRecCosts <- subset(threeCapRecData, select = c(val))
+# paste("avg costs of 3CapRec: ", mean(threeCapRecCosts[["val"]]))
 ##############################################################################
 
 ggsave(plotPointsPre, file="solver_instance_cost.png", width = 8, height = 5)
