383b4767cc6ec23033bc6871ded9aa610dd4ae8e
tbohne <tbohne@uos.de>
2019-01-22 16:31:39 +0100

    implemented python script to compute the percentage deviation from the optimal value

---
+++ StorageLoadingProblems/src/SLP/r_scripts/percentage_deviation.py
@@ -0,0 +1,26 @@
+import sys
+
+if __name__ == '__main__':
+
+    time_limit = 5.0
+
+    input = sys.stdin.readlines()
+    optimal_costs = []
+
+    for line in input:
+        if "BinP," in line:
+            runtime = float(line.split(",")[2])
+            if runtime <= time_limit:
+                optimal_costs.append(float(line.split(",")[3]))
+            else:
+                optimal_costs.append(-1.0)
+                print("Problem: Not all optimal cost values have been determined.")
+
+    for line in input:
+        if "3Cap," in line:
+            instance_idx = line.split(",")[0].split("_")[3]
+            three_cap_val = float(line.split(",")[3].strip())
+            optimal_val = optimal_costs[int(instance_idx)]
+
+            diff = abs(optimal_val - three_cap_val)
+            print("percentage deviation for instance " + instance_idx + ": " + str(round((diff / (optimal_val) * 100), 2)))
