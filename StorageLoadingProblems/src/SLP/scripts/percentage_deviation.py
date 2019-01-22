86340f91cc4a3f1dc37bbc714ae915a9cd433d6f
tbohne <tbohne@uos.de>
2019-01-22 23:30:19 +0100

    fixed several issues in percentage deviation computation

--- StorageLoadingProblems/src/SLP/scripts/percentage_deviation.py
+++ StorageLoadingProblems/src/SLP/scripts/percentage_deviation.py
@@ -25,22 +25,26 @@ def read_optimal_costs():
             optimal_costs.append(float(line.strip()))
     return optimal_costs
 
-def read_time_limit():
-    costs_file = open("optimal_costs.txt", "r")
-    lines = costs_file.readlines()
-    costs_file.close()
+def get_time_limit():
+    for filename in os.listdir("../../../res/solutions/"):
+        if "00.txt" in filename:
+            instance_file = open("../../../res/solutions/" + filename, "r")
+            break
 
-    optimal_costs = []
+    lines = instance_file.readlines()
     for line in lines:
-        if "time limit" in line:
-            return int(line.split(":")[1].strip())
+        if "time limit:" in line:
+            time_limit = int(line.split(" ")[2].strip())
+            break
+    instance_file.close()
+    return time_limit
 
 if __name__ == '__main__':
 
     solutions_file = open("../../../res/solutions/solutions.csv", "r")
     lines = solutions_file.readlines()
     optimal_costs = read_optimal_costs()
-    time_limit = read_time_limit()
+    time_limit = get_time_limit()
 
     optimally_solved_by_bin_packing = 0
     optimally_solved_by_three_index = 0
@@ -61,6 +65,10 @@ if __name__ == '__main__':
     sum_of_deviations_three_idx = 0.0
     sum_of_deviations_three_cap = 0.0
 
+    bin_packing_solutions = 0
+    three_index_solutions = 0
+    three_cap_solutions = 0
+
     for line in lines:
 
         if "BinP," in line or "3Idx," in line or "3Cap," in line:
@@ -71,15 +79,18 @@ if __name__ == '__main__':
 
         if "BinP," in line:
             sum_of_deviations_bin_packing += percentage_deviation
+            bin_packing_solutions += 1
         elif "3Idx," in line:
             sum_of_deviations_three_idx += percentage_deviation
+            three_index_solutions += 1
         elif "3Cap," in line:
             sum_of_deviations_three_cap += percentage_deviation
+            three_cap_solutions += 1
 
     num_of_instances = int(instance_idx) + 1
-    print("avg percentage deviation BinP: " + str(round((sum_of_deviations_bin_packing / num_of_instances), 2)))
-    print("avg percentage deviation 3Idx: " + str(round((sum_of_deviations_three_idx / num_of_instances), 2)))
-    print("avg percentage deviation 3Cap: " + str(round((sum_of_deviations_three_cap / num_of_instances), 2)))
+    print("avg percentage deviation BinP: " + str(round((sum_of_deviations_bin_packing / bin_packing_solutions), 2)))
+    print("avg percentage deviation 3Idx: " + str(round((sum_of_deviations_three_idx / three_index_solutions), 2)))
+    print("avg percentage deviation 3Cap: " + str(round((sum_of_deviations_three_cap / three_cap_solutions), 2)))
     print()
     print("optimally solved by BinP: " + str((optimally_solved_by_bin_packing / num_of_instances) * 100) + " %")
     print("optimally solved by 3Idx: " + str((optimally_solved_by_three_index / num_of_instances) * 100) + " %")
