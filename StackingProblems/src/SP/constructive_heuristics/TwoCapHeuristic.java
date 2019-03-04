5128ce76bec4b60472a2ef5ccaaedb048b9193a6
tbohne <tbohne@uos.de>
2019-03-04 16:30:49 +0100

    TwoCapHeuristic - refactoring / cleaning up / adding doc

--- StackingProblems/src/SP/constructive_heuristics/TwoCapHeuristic.java
+++ StackingProblems/src/SP/constructive_heuristics/TwoCapHeuristic.java
@@ -1,5 +1,6 @@
 package SP.constructive_heuristics;
 
+import SP.representations.BipartiteGraph;
 import SP.representations.Instance;
 import SP.representations.MCMEdge;
 import SP.representations.Solution;
@@ -66,23 +67,21 @@ public class TwoCapHeuristic {
      * @param unmatchedItems - the items that are assigned to their own stack
      * @return the generated bipartite graph
      */
-    public DefaultUndirectedWeightedGraph<String, DefaultWeightedEdge> generateBipartiteGraph(
-        ArrayList<MCMEdge> itemPairs, ArrayList<Integer> unmatchedItems
-    ) {
-        BipartitePartitioning<String, DefaultWeightedEdge> bipartiteGraph = new DefaultUndirectedWeightedGraph<>(DefaultWeightedEdge.class);
+    public BipartiteGraph generateBipartiteGraph(ArrayList<MCMEdge> itemPairs, ArrayList<Integer> unmatchedItems) {
+        DefaultUndirectedWeightedGraph<String, DefaultWeightedEdge> graph = new DefaultUndirectedWeightedGraph<>(DefaultWeightedEdge.class);
         Set<String> partitionOne = new HashSet<>();
         Set<String> partitionTwo = new HashSet<>();
 
-        GraphUtil.addVerticesForItemPairs(itemPairs, bipartiteGraph, partitionOne);
-        GraphUtil.addVerticesForUnmatchedItems(unmatchedItems, bipartiteGraph, partitionOne);
-        GraphUtil.addVerticesForStacks(this.instance.getStacks(), bipartiteGraph, partitionTwo);
+        GraphUtil.addVerticesForItemPairs(itemPairs, graph, partitionOne);
+        GraphUtil.addVerticesForUnmatchedItems(unmatchedItems, graph, partitionOne);
+        GraphUtil.addVerticesForStacks(this.instance.getStacks(), graph, partitionTwo);
 
-        ArrayList<Integer> dummyItems = GraphUtil.introduceDummyVertices(bipartiteGraph, partitionOne, partitionTwo);
-        GraphUtil.addEdgesForItemPairs(bipartiteGraph, itemPairs, this.instance.getStacks(), this.instance.getCosts());
-        GraphUtil.addEdgesForUnmatchedItems(bipartiteGraph, unmatchedItems, this.instance.getStacks(), this.instance.getCosts());
-        GraphUtil.addEdgesForDummyItems(bipartiteGraph, dummyItems, this.instance.getStacks());
+        ArrayList<Integer> dummyItems = GraphUtil.introduceDummyVertices(graph, partitionOne, partitionTwo);
+        GraphUtil.addEdgesForItemPairs(graph, itemPairs, this.instance.getStacks(), this.instance.getCosts());
+        GraphUtil.addEdgesForUnmatchedItems(graph, unmatchedItems, this.instance.getStacks(), this.instance.getCosts());
+        GraphUtil.addEdgesForDummyItems(graph, dummyItems, this.instance.getStacks());
 
-        return bipartiteGraph;
+        return new BipartiteGraph(partitionOne, partitionTwo, graph);
     }
 
     /**
@@ -126,8 +125,11 @@ public class TwoCapHeuristic {
      * @return the generated solution
      */
     public Solution solve() {
+
         Solution sol = new Solution();
+
         if (this.instance.getStackCapacity() == 2) {
+
             this.startTime = System.currentTimeMillis();
 
             DefaultUndirectedGraph stackingConstraintGraph = GraphUtil.generateStackingConstraintGraph(
@@ -137,19 +139,17 @@ public class TwoCapHeuristic {
                 (Integer.MAX_VALUE / this.instance.getItems().length),
                 this.instance.getStacks()
             );
-
             EdmondsMaximumCardinalityMatching<String, DefaultEdge> itemMatching = new EdmondsMaximumCardinalityMatching<>(stackingConstraintGraph);
             ArrayList<MCMEdge> itemPairs = GraphUtil.parseItemPairFromMCM(itemMatching);
             ArrayList<Integer> unmatchedItems = HeuristicUtil.getUnmatchedItemsFromPairs(itemPairs, this.instance.getItems());
 
-
-            DefaultUndirectedWeightedGraph<String, DefaultWeightedEdge> bipartiteGraph = this.generateBipartiteGraph(itemPairs, unmatchedItems);
-            KuhnMunkresMinimalWeightBipartitePerfectMatching mwpm = new KuhnMunkresMinimalWeightBipartitePerfectMatching(
-                    bipartiteGraph, partitionOne, partitionTwo
+            BipartiteGraph bipartiteGraph = this.generateBipartiteGraph(itemPairs, unmatchedItems);
+            KuhnMunkresMinimalWeightBipartitePerfectMatching minCostPerfectMatching = new KuhnMunkresMinimalWeightBipartitePerfectMatching(
+                bipartiteGraph.getGraph(), bipartiteGraph.getPartitionOne(), bipartiteGraph.getPartitionTwo()
             );
-
             this.parseMatchingAndAssignItems(minCostPerfectMatching);
             this.fixOrderInStacks();
+
             sol = new Solution((System.currentTimeMillis() - startTime) / 1000.0, this.timeLimit, this.instance);
         } else {
             System.out.println("This heuristic is designed to solve SP with a stack capacity of 2.");
