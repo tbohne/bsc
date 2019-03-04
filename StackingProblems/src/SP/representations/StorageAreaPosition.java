ce57042da9f6bc97cc99bf57699b60173ed8921a
tbohne <tbohne@uos.de>
2019-03-04 11:27:06 +0100

    StorageAreaPosition - refactoring / cleaning up / adding doc

--- StackingProblems/src/SP/representations/StorageAreaPosition.java
+++ StackingProblems/src/SP/representations/StorageAreaPosition.java
@@ -1,19 +1,40 @@
 package SP.representations;
 
+/**
+ * Represents a position in the storage area.
+ *
+ * @author Tim Bohne
+ */
 public class StorageAreaPosition {
 
     private int stackIdx;
     private int level;
 
+    /**
+     * Constructor
+     *
+     * @param stackIdx - the index of the stack
+     * @param level    - the level inside the stack
+     */
     public StorageAreaPosition(int stackIdx, int level) {
         this.stackIdx = stackIdx;
         this.level = level;
     }
 
+    /**
+     * Returns the position's stack index.
+     *
+     * @return the position's stack index
+     */
     public int getStackIdx() {
         return this.stackIdx;
     }
 
+    /**
+     * Returns the position's level inside the stack.
+     *
+     * @return the position's level inside the stack
+     */
     public int getLevel() {
         return this.level;
     }
