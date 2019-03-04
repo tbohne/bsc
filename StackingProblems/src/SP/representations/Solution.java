59770b25184f9df2843b652bdb95c2876576a20a
tbohne <tbohne@uos.de>
2019-03-04 11:04:36 +0100

    Solution - refactoring / cleaning up / adding doc

--- StackingProblems/src/SP/representations/Solution.java
+++ StackingProblems/src/SP/representations/Solution.java
@@ -1,93 +1,137 @@
 package SP.representations;
 
+import SP.util.RepresentationUtil;
+
 import java.util.ArrayList;
 
+/**
+ * Represents a solution to a stacking problem.
+ *
+ * @author Tim Bohne
+ */
 public class Solution {
 
+    private String nameOfSolvedInstance;
+    private Instance solvedInstance;
     private double timeToSolve;
     private int timeLimit;
     private double objectiveValue;
-
     private int[][] filledStorageArea;
     private boolean empty;
 
-    private String nameOfSolvedInstance;
-
-    private Instance solvedInstance;
-
-    private int numberOfItems;
-
+    /**
+     * Constructor for empty solutions.
+     */
     public Solution() {
         this.empty = true;
     }
 
+    /**
+     * Constructor receiving the objective value.
+     *
+     * @param timeToSolve    - the time it took to create the solution
+     * @param objectiveValue - the solution's objective value
+     * @param timeLimit      - the considered time limit
+     * @param solvedInstance - the solved instance
+     */
     public Solution(double timeToSolve, double objectiveValue, int timeLimit, Instance solvedInstance) {
         this.timeToSolve = timeToSolve;
         this.objectiveValue = objectiveValue;
         this.empty = false;
         this.nameOfSolvedInstance = solvedInstance.getName();
         this.timeLimit = timeLimit;
-        this.numberOfItems = solvedInstance.getItems().length;
-
         this.solvedInstance = solvedInstance;
-
-        this.filledStorageArea = new int[solvedInstance.getStacks().length][];
-        for (int i = 0; i < solvedInstance.getStacks().length; i++) {
-            this.filledStorageArea[i] = solvedInstance.getStacks()[i].clone();
-        }
+        this.createFilledStorageAreaFromInstance();
     }
 
+    /**
+     * Constructor not receiving the objective value.
+     *
+     * @param timeToSolve    - the time it took to create the solution
+     * @param timeLimit      - the considered time limit
+     * @param solvedInstance - the solved instance
+     */
     public Solution(double timeToSolve, int timeLimit, Instance solvedInstance) {
         this.timeToSolve = timeToSolve;
         this.empty = false;
         this.nameOfSolvedInstance = solvedInstance.getName();
         this.timeLimit = timeLimit;
-        this.numberOfItems = solvedInstance.getItems().length;
-
         this.solvedInstance = solvedInstance;
-
-        this.filledStorageArea = new int[solvedInstance.getStacks().length][];
-        for (int i = 0; i < solvedInstance.getStacks().length; i++) {
-            this.filledStorageArea[i] = solvedInstance.getStacks()[i].clone();
-        }
-
-        this.objectiveValue = this.getCost();
+        this.createFilledStorageAreaFromInstance();
+        this.objectiveValue = this.computeCosts();
     }
 
+    /**
+     * Copy-Constructor
+     *
+     * @param sol - the solution to be copied
+     */
     public Solution(Solution sol) {
-        this.timeToSolve = sol.timeToSolve;
-        this.objectiveValue = sol.objectiveValue;
-        this.empty = sol.empty;
-        this.numberOfItems = sol.numberOfItems;
+        this.timeToSolve = sol.getTimeToSolveAsDouble();
+        this.objectiveValue = sol.getObjectiveValueAsDouble();
+        this.empty = sol.isEmpty();
         this.solvedInstance = new Instance(sol.solvedInstance);
-        this.nameOfSolvedInstance = sol.nameOfSolvedInstance;
-        this.timeLimit = sol.timeLimit;
+        this.nameOfSolvedInstance = sol.getNameOfSolvedInstance();
+        this.timeLimit = sol.getTimeLimit();
+
         this.filledStorageArea = new int[sol.solvedInstance.getStacks().length][];
         for (int i = 0; i < sol.getFilledStorageArea().length; i++) {
             this.filledStorageArea[i] = sol.getFilledStorageArea()[i].clone();
         }
     }
 
+    /**
+     * Creates the filled storage area from the instance's stack assignments.
+     */
+    public void createFilledStorageAreaFromInstance() {
+        this.filledStorageArea = new int[this.solvedInstance.getStacks().length][];
+        for (int i = 0; i < this.solvedInstance.getStacks().length; i++) {
+            this.filledStorageArea[i] = this.solvedInstance.getStacks()[i].clone();
+        }
+    }
+
+    /**
+     * Returns the filled storage area.
+     *
+     * @return the filled storage area
+     */
     public int[][] getFilledStorageArea() {
         return this.filledStorageArea;
     }
 
+    /**
+     * Returns the considered time limit.
+     *
+     * @return the considered time limit
+     */
+    public int getTimeLimit() {
+        return this.timeLimit;
+    }
+
+    /**
+     * Returns whether the solution is empty.
+     *
+     * @return whether the solution is empty
+     */
     public boolean isEmpty() {
         return this.empty;
     }
 
-    // TODO: fix infinite loop for invalid stack assignments
-    public void transformStackAssignmentIntoValidSolutionIfPossible() {
-        for (int stack = 0; stack < this.filledStorageArea.length; stack++) {
+    /**
+     * Transforms the given stack assignments into a feasible solution by reorganizing
+     * the items in a way that respects the stacking constraints.
+     */
+    public void transformStackAssignmentsIntoValidSolutionIfPossible() {
 
+        // TODO: revise
+
+        for (int stack = 0; stack < this.filledStorageArea.length; stack++) {
             boolean somethingChanged = true;
 
             while (somethingChanged) {
-
                 somethingChanged = false;
 
                 for (int item = 0; item < this.solvedInstance.getStackCapacity() - 1; item++) {
-
                     if (this.filledStorageArea[stack][item] != -1 && this.filledStorageArea[stack][item + 1] != -1) {
                         if (this.solvedInstance.getStackingConstraints()[this.filledStorageArea[stack][item]][this.filledStorageArea[stack][item + 1]] == 0) {
                             int tmp = this.filledStorageArea[stack][item];
@@ -102,10 +146,13 @@ public class Solution {
         }
     }
 
+    /**
+     * Returns whether all of the instance's items have been assigned to a stack.
+     *
+     * @return whether all items have been assigned to a stack
+     */
     public boolean allItemsAssigned() {
-
-        boolean[] allItemsAssigned = new boolean[this.numberOfItems];
-
+        boolean[] allItemsAssigned = new boolean[this.solvedInstance.getItems().length];
         for (int i = 0; i < this.filledStorageArea.length; i++) {
             for (int j = 0; j < this.filledStorageArea[i].length; j++) {
                 if (this.filledStorageArea[i][j] != -1) {
@@ -113,7 +160,6 @@ public class Solution {
                 }
             }
         }
-
         for (boolean itemAssigned : allItemsAssigned) {
             if (!itemAssigned) {
                 return false;
@@ -122,14 +168,16 @@ public class Solution {
         return true;
     }
 
+    /**
+     * Returns whether the stacking constraints are respected by the solution's stack assignments.
+     *
+     * @return whether the solution is respecting the stacking constraints
+     */
     public boolean stackingConstraintsRespected() {
-
         for (int stack = 0; stack < this.filledStorageArea.length; stack++) {
             for (int level = 1; level < this.filledStorageArea[stack].length; level++) {
-
                 int itemBelow = this.filledStorageArea[stack][level];
                 int itemAbove = this.filledStorageArea[stack][level - 1];
-
                 if (itemAbove != -1 && itemBelow != -1) {
                     if (this.solvedInstance.getStackingConstraints()[itemAbove][itemBelow] != 1) {
                         return false;
@@ -140,12 +188,15 @@ public class Solution {
         return true;
     }
 
-    public boolean stackConstraintsRespected() {
+    /**
+     * Returns whether the placement constraints are respected by the solution's stack assignments.
+     *
+     * @return whether the solution is respecting the placement constraints
+     */
+    public boolean placementConstraintsRespected() {
         for (int stack = 0; stack < this.filledStorageArea.length; stack++) {
             for (int level = 0; level < this.filledStorageArea[stack].length; level++) {
-
                 int item = this.filledStorageArea[stack][level];
-
                 if (item != -1) {
                     if (this.solvedInstance.getCosts()[item][stack] >= Integer.MAX_VALUE / this.solvedInstance.getItems().length) {
                         return false;
@@ -156,6 +207,11 @@ public class Solution {
         return true;
     }
 
+    /**
+     * Returns whether the solution contains duplicates (items occurring more than once).
+     *
+     * @return whether the solution contains duplicate items
+     */
     public boolean containsDuplicates() {
         ArrayList<Integer> assignedItems = new ArrayList<>();
         for (int i = 0; i < this.filledStorageArea.length; i++) {
@@ -171,24 +227,37 @@ public class Solution {
         return false;
     }
 
+    /**
+     * Sets the solution's time to solve the instance.
+     *
+     * @param duration - the duration it took to solve the instance
+     */
     public void setTimeToSolve(double duration) {
         this.timeToSolve = duration;
     }
 
+    /**
+     * Returns whether the solution is feasible which means that all items have been
+     * assigned to a position in a stack in a way that respects the stacking- and
+     * placement-constraints. Additionally no item have been assigned to more than one position.
+     *
+     * @return whether the solution is feasible
+     */
     public boolean isFeasible() {
-
         if (!this.empty) {
-            System.out.println("all items: " + this.allItemsAssigned());
-            System.out.println("stacking: " + this.stackingConstraintsRespected());
-            System.out.println("stack: " + this.stackConstraintsRespected());
+            System.out.println("all items assigned: " + this.allItemsAssigned());
+            System.out.println("stacking constraints resp.: " + this.stackingConstraintsRespected());
+            System.out.println("placement constraints resp.: " + this.placementConstraintsRespected());
             System.out.println("items assigned: " + this.getNumberOfAssignedItems());
             System.out.println("contains duplicates: " + this.containsDuplicates());
         }
-
         return !this.empty && this.allItemsAssigned() && this.stackingConstraintsRespected()
-            && this.stackConstraintsRespected() && !this.containsDuplicates();
+            && this.placementConstraintsRespected() && !this.containsDuplicates();
     }
 
+    /**
+     * Prints the solution's storage area.
+     */
     public void printStorageArea() {
         for (int i = 0; i < this.getFilledStorageArea().length; i++) {
             for (int j = 0; j < this.getFilledStorageArea()[i].length; j++) {
@@ -198,14 +267,16 @@ public class Solution {
         }
     }
 
+    /**
+     * Returns the solution's number of assigned items.
+     *
+     * @return the solution's number of assigned items
+     */
     public int getNumberOfAssignedItems() {
-
         if (this.empty) {
             return 0;
         }
-
         int numberOfAssignedItems = 0;
-
         for (int i = 0; i < this.filledStorageArea.length; i++) {
             for (int j = 0; j < this.filledStorageArea[i].length; j++) {
                 if (this.filledStorageArea[i][j] != -1) {
@@ -213,79 +284,80 @@ public class Solution {
                 }
             }
         }
-
         return numberOfAssignedItems;
     }
 
-    public int getCost() {
-
+    /**
+     * Computes the solution's costs.
+     *
+     * @return the solution's costs
+     */
+    public int computeCosts() {
         if (this.empty) {
             return 9999999;
         }
-
-        int cost = 0;
-
+        int costs = 0;
         for (int stack = 0; stack < this.filledStorageArea.length; stack++) {
             for (int level = 0; level < this.filledStorageArea[stack].length; level++) {
                 int item = this.filledStorageArea[stack][level];
                 if (item != -1) {
-                    cost += this.solvedInstance.getCosts()[item][stack];
+                    costs += this.solvedInstance.getCosts()[item][stack];
                 }
             }
         }
-        return cost;
+        return costs;
     }
 
+    /**
+     * Returns a string representation of the solutions time to solve the instance.
+     *
+     * @return the solution's time to solve the instance
+     */
     public String getTimeToSolve() {
         return String.format("%.02f", this.timeToSolve).replace(",", ".");
     }
 
+    /**
+     * Returns the solution's time to solve the instance.
+     *
+     * @return the solution's time to solve the instance
+     */
+    public double getTimeToSolveAsDouble() {
+        return this.timeToSolve;
+    }
+
+    /**
+     * Returns the string representation of the solution's objective value.
+     *
+     * @return the solution's objective value
+     */
     public String getObjectiveValue() {
         return String.format("%.02f", this.objectiveValue).replace(",", ".");
     }
 
+    /**
+     * Returns the solution's objective value.
+     *
+     * @return the solution's objective value
+     */
     public double getObjectiveValueAsDouble() {
         return this.objectiveValue;
     }
 
+    /**
+     * Returns the name of the solved instance.
+     *
+     * @return the name of the solved instance
+     */
     public String getNameOfSolvedInstance() {
         return this.nameOfSolvedInstance;
     }
 
-    public int getMaximumStringOffset(int numberOfStacks) {
-        int cnt = 0;
-        while ((numberOfStacks / 10) > 0) {
-            numberOfStacks /= 10;
-            cnt++;
-        }
-        return cnt;
-    }
-
-    public String getCurrentSpace(int idx, int maxOffset) {
-        String space = "";
-        if (idx < 10) {
-            for (int i = 0; i < maxOffset; i++) {
-                space += " ";
-            }
-            return space;
-        } else if (idx < 100) {
-            for (int i = 0; i < maxOffset - 1; i++) {
-                space += " ";
-            }
-            return space;
-        } else if (idx < 1000) {
-            for (int i = 0; i < maxOffset - 2; i++) {
-                space += " ";
-            }
-            return space;
-        } else {
-            for (int i = 0; i < maxOffset - 3; i++) {
-                space += " ";
-            }
-            return space;
-        }
-    }
-
+    /**
+     * Returns a string visualizing the solution.
+     *
+     * @return a string visualizing the solution
+     */
     public String toString() {
 
         String str = "";
@@ -298,12 +370,10 @@ public class Solution {
             str += this.timeToSolve > this.timeLimit ? " (not optimal)\n\n" : "\n\n";
             str += "stacks (top to bottom):\n";
 
-            int maxStringOffset = getMaximumStringOffset(this.filledStorageArea.length);
+            int maxStringOffset = RepresentationUtil.getMaximumStringOffset(this.filledStorageArea.length);
 
             for (int i = 0; i < this.filledStorageArea.length; i++) {
-
-                String space = getCurrentSpace(i, maxStringOffset);
-
+                String space = RepresentationUtil.getCurrentSpace(i, maxStringOffset);
                 str += "stack " + space + i + ":    ";
                 for (int j = 0; j < this.filledStorageArea[i].length; j++) {
                     if (this.filledStorageArea[i][j] != -1) {
