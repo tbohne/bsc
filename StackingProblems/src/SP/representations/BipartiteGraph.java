bc7fa1707b753919d46b39ccc2f2999a62eaf891
tbohne <tbohne@uos.de>
2019-03-04 16:17:17 +0100

    implemented bipartite graph class to be used in the heuristics

---
+++ StackingProblems/src/SP/representations/BipartiteGraph.java
@@ -0,0 +1,64 @@
+package SP.representations;
+
+import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+import java.util.Set;
+
+// TODO: should only be creatable if the requirements of a bipartite graph are given
+
+/**
+ * Represents a bipartite graph consisting of two partitions.
+ *
+ * @author Tim Bohne
+ */
+public class BipartiteGraph {
+
+    private Set<String> partitionOne;
+    private Set<String> partitionTwo;
+    private DefaultUndirectedWeightedGraph<String, DefaultWeightedEdge> graph;
+
+    /**
+     * Constructor
+     *
+     * @param partitionOne - the first partition of the bipartite graph
+     * @param partitionTwo - the second partition of the bipartite graph
+     * @param graph        - the actual graph
+     */
+    public BipartiteGraph(
+        Set<String> partitionOne,
+        Set<String> partitionTwo,
+        DefaultUndirectedWeightedGraph<String, DefaultWeightedEdge> graph
+    ) {
+        this.partitionOne = partitionOne;
+        this.partitionTwo = partitionTwo;
+        this.graph = graph;
+    }
+
+    /**
+     * Returns the bipartite graph's first partition.
+     *
+     * @return the bipartite graph's first partition
+     */
+    public Set<String> getPartitionOne() {
+        return this.partitionOne;
+    }
+
+    /**
+     * Returns the bipartite graph's second partition.
+     *
+     * @return the bipartite graph's second partition
+     */
+    public Set<String> getPartitionTwo() {
+        return this.partitionTwo;
+    }
+
+    /**
+     * Returns the bipartite graph.
+     *
+     * @return the bipartite graph
+     */
+    public DefaultUndirectedWeightedGraph<String, DefaultWeightedEdge> getGraph() {
+        return this.graph;
+    }
+}
