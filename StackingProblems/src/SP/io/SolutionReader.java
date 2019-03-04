d449dceb72a45b564512bdd831589a34cd2303f1
tbohne <tbohne@uos.de>
2019-03-04 11:17:30 +0100

    SolutionReader - refactoring / cleaning up / adding doc

--- StackingProblems/src/SP/io/SolutionReader.java
+++ StackingProblems/src/SP/io/SolutionReader.java
@@ -11,17 +11,63 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Provides functionalities to read instances of stacking problems from the file system.
+ *
+ * @author Tim Bohne
+ */
 public class SolutionReader {
 
+    /**
+     * Reads the storage area from the file.
+     *
+     * @param br          - the buffered reader pointing to the specific line
+     * @param storageArea - the storage area to be filled
+     * @throws IOException
+     */
+    public static void readStorageArea(BufferedReader br, ArrayList<ArrayList<Integer>> storageArea) throws IOException {
+        for (String line; (line = br.readLine()) != null; ) {
+            ArrayList<Integer> stack = new ArrayList<>();
+            String first = line.split(":")[1].trim();
+            if (first.length() > 0) {
+                int[] array = Arrays.stream(first.split("\\s")).mapToInt(Integer::parseInt).toArray();
+                for (int item : array) {
+                    stack.add(item);
+                }
+            }
+            storageArea.add(new ArrayList<>(stack));
+        }
+    }
+
+    /**
+     * Copies the read storage area to the solution's filled storage area.
+     * TODO: not the best way to do that
+     *
+     * @param storageArea - the storage area to be copied
+     * @param sol         - the solution whose storage area gets filled
+     */
+    public static void fillStorageArea(ArrayList<ArrayList<Integer>> storageArea, Solution sol) {
+        for (int i = 0; i < storageArea.size(); i++) {
+            for (int j = 0; j < storageArea.get(i).size(); j++) {
+                sol.getFilledStorageArea()[i][j] = storageArea.get(i).get(j);
+            }
+        }
+    }
+
+    /**
+     * Reads the solutions from the files contained in the specified directory.
+     *
+     * @param solutionDirName - name of the directory containing the solutions
+     * @param instanceDirName - name of the directory containing the solved instances
+     * @return a list of the read solutions
+     */
     public static ArrayList<Solution> readSolutionsFromDir(String solutionDirName, String instanceDirName) {
 
         File dir = new File(solutionDirName);
         ArrayList<Solution> solutions = new ArrayList<>();
 
         for (File file : dir.listFiles()) {
-
             if (!file.isDirectory() && file.getName().contains("slp_")) {
-
                 try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                     for (String line; (line = br.readLine()) != null; ) {
                         // then the next lines should be used to read the solution
@@ -29,7 +75,6 @@ public class SolutionReader {
                             break;
                         }
                     }
-
                     String line = br.readLine();
                     int timeLimit = Integer.parseInt(line.split(":")[1].replace("s", "").trim());
                     line = br.readLine();
@@ -40,31 +85,15 @@ public class SolutionReader {
                     br.readLine();
 
                     ArrayList<ArrayList<Integer>> storageArea = new ArrayList<>();
-
-                    for (String l; (l = br.readLine()) != null; ) {
-                        ArrayList<Integer> stack = new ArrayList<>();
-                        String first = l.split(":")[1].trim();
-                        if (first.length() > 0) {
-                            int[] array = Arrays.stream(first.split("\\s")).mapToInt(Integer::parseInt).toArray();
-                            for (int item : array) {
-                                stack.add(item);
-                            }
-                        }
-                        storageArea.add(new ArrayList<>(stack));
-                    }
+                    readStorageArea(br, storageArea);
 
                     String instanceName = file.getName().replace("sol", "instance");
-
                     Instance instance = InstanceReader.readInstance(instanceDirName + instanceName);
                     Solution sol = new Solution(timeToSolveInSeconds, objectiveValue, timeLimit, instance);
-
-                    for (int i = 0; i < storageArea.size(); i++) {
-                        for (int j = 0; j < storageArea.get(i).size(); j++) {
-                            sol.getFilledStorageArea()[i][j] = storageArea.get(i).get(j);
-                        }
-                    }
+                    fillStorageArea(storageArea, sol);
 
                     solutions.add(sol);
+                    br.close();
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -73,5 +102,4 @@ public class SolutionReader {
         }
         return solutions;
     }
-
 }
