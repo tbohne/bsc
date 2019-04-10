65e80f2af36c77282e98f2937460f311b951c8a4
tbohne <tbohne@uos.de>
2019-04-10 11:25:39 +0200

    3Cap - added config parameter for post-processing

--- StackingProblems/src/SP/constructive_heuristics/ConstructiveHeuristicTest.java
+++ StackingProblems/src/SP/constructive_heuristics/ConstructiveHeuristicTest.java
@@ -13,14 +13,15 @@ public class ConstructiveHeuristicTest {
 
     public static final int TIME_LIMIT = 300;
     public static final boolean POST_PROCESSING = true;
+    public static final boolean PRIORITZIE_RUNTIME = false;
 
     public static void main (String[] args) {
 
-        Instance instance = InstanceReader.readInstance("res/instances/slp_instance_500_300_2_17.txt");
+        Instance instance = InstanceReader.readInstance("res/instances/slp_instance_500_201_3_00.txt");
         System.out.println("working on: " + instance.getName());
 
-        TwoCapHeuristic solver = new TwoCapHeuristic(instance, TIME_LIMIT);
-        Solution sol = solver.solve(POST_PROCESSING);
+        ThreeCapHeuristic solver = new ThreeCapHeuristic(instance, TIME_LIMIT);
+        Solution sol = solver.solve(PRIORITZIE_RUNTIME,  POST_PROCESSING);
 
         System.out.println("feasible: " + sol.isFeasible());
         System.out.println("cost: " + sol.computeCosts());
