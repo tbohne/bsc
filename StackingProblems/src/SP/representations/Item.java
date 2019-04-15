29e34ca8da13f3a7caf6baccac771cd6fdfdfcf0
tbohne <tbohne@uos.de>
2019-04-15 14:03:38 +0200

    Item - added position and copy constructor

--- StackingProblems/src/SP/representations/Item.java
+++ StackingProblems/src/SP/representations/Item.java
@@ -10,6 +10,7 @@ public class Item {
     private int idx;
     private float length;
     private float width;
+    private Position pos;
 
     /**
      * Constructor
@@ -17,11 +18,25 @@ public class Item {
      * @param idx    - the index that identifies the item
      * @param length - the length of the item
      * @param width  - the width of the item
+     * @param pos    - the position of the item on the vehicle
      */
-    public Item(int idx, float length, float width) {
+    public Item(int idx, float length, float width, Position pos) {
         this.idx = idx;
         this.length = length;
         this.width = width;
+        this.pos = pos;
+    }
+
+    /**
+     * Copy-Constructor
+     *
+     * @param item - the item to be copied
+     */
+    public Item(Item item) {
+        this.idx = item.getIdx();
+        this.length = item.getLength();
+        this.width = item.getWidth();
+        this.pos = new Position(item.getPosition());
     }
 
     /**
@@ -49,5 +64,14 @@ public class Item {
      */
     public float getWidth() {
         return this.width;
+    }
+
+    /**
+     * Returns the item's position on the vehicle.
+     *
+     * @return the item's position
+     */
+    public Position getPosition() {
+        return this.pos;
     }
 }
