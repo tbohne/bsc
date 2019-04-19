b61f3918d022c13cf57a5e4e3c2fa396c469da21
tbohne <tbohne@uos.de>
2019-04-19 17:34:57 +0200

    TabuSearchTest - moved config

--- StackingProblems/src/SP/post_optimization_methods/TabuSearchTest.java
+++ StackingProblems/src/SP/post_optimization_methods/TabuSearchTest.java
@@ -7,25 +7,16 @@ import java.util.ArrayList;
 
 public class TabuSearchTest {
 
-    /******** CONFIGURABLE TS OPTIONS ************************/
-    // TL clears otherwise
-    public static final boolean ITERATIONS_CRITERION = true;
-    // best fit otherwise
-    public static final boolean FIRST_FIT = true;
-    public static final boolean ONLY_FEASIBLE = true;
-    /*********************************************************/
-    public static final String SOLVER = "TwoCapHeuristic";
-
     public static void main(String[] args) {
         ArrayList<Solution> solutions = SolutionReader.readSolutionsFromDir(
-            "res/solutions/", "res/instances/", SOLVER
+            "res/solutions/", "res/instances/", "TwoCapHeuristic"
         );
         Solution sol = solutions.get(0);
         System.out.println("solved instance: " + sol.getNameOfSolvedInstance());
 
         TabuSearch ts = new TabuSearch(sol);
         System.out.println("COSTS BEFORE: " + sol.computeCosts());
-        sol = ts.solve(ITERATIONS_CRITERION, FIRST_FIT, ONLY_FEASIBLE);
+        sol = ts.solve();
         System.out.println("FINAL COSTS: " + sol.computeCosts());
     }
 }
