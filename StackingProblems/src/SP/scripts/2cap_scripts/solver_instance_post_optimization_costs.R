7e94626230ff4a3ef8d8afa36d1ad58152d011e2
tbohne <tbohne@uos.de>
2019-05-15 15:57:20 +0200

    added R scripts for TS results (to be refined later)

---
+++ StackingProblems/src/SP/scripts/2cap_scripts/solver_instance_post_optimization_costs.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+
+input <- read.csv(file = "../../../../res/solutions/solutions_imp.csv", header = TRUE, sep = ",")
+solverEntries <- subset(input, solver == "TS" | solver == "OPT" | solver == "2Cap")
+
+plotPointsPre <- ggplot(data = solverEntries, aes(x = val, y = instance, color = solver, group = solver))
+scaledPlot <- plotPointsPre + geom_point() + xlab("costs") + ylab("instance") #+ scale_x_continuous(limits = c(6500, 6750))
+finalPlot <- scaledPlot + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
+
+ggsave(finalPlot, file = "solver_instance_cost.png", width=6, height=4)
