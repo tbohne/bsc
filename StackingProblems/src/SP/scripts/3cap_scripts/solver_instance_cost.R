c9958cdc00a1ff38fc406db81959200f59ac2620
tbohne <tbohne@uos.de>
2019-05-22 21:20:04 +0200

    removed outdated scripts

--- StackingProblems/src/SP/scripts/3cap_scripts/solver_instance_cost.R
+++ StackingProblems/src/SP/scripts/3cap_scripts/solver_instance_cost.R
@@ -2,7 +2,16 @@ library(ggplot2)
 
 input <- read.csv(file = "../../../../res/solutions/solutions.csv", header = TRUE, sep = ",")
 solverEntries <- subset(input, solver == "BinP" | solver == "3Idx" | solver == "3Cap" | solver == "LB")
-plotPointsPre <- ggplot(data = solverEntries, aes(x = val, y = instance, color = solver, group = solver)) + geom_point() + xlab("costs") + ylab("instance")
+
+# LOG SCALING
+# breaks <- c(211700, 211850, 212850, 220000, 270000)
+# baseline = 211650
+# plotPointsPre <- ggplot(data = solverEntries, aes(x = val - baseline, y = instance, color = solver, group = solver))
+# scaledPlot <- plotPointsPre + geom_point() + xlab("costs") + ylab("instance") + scale_x_log10(breaks = breaks - baseline, labels = breaks)
+
+# # DEFAULT
+plotPointsPre <- ggplot(data = solverEntries, aes(x = val, y = instance, color = solver, group = solver))
+scaledPlot <- plotPointsPre + geom_point() + xlab("costs") + ylab("instance") #+ scale_x_continuous(limits = c(6500, 6750))
 
 ##############################################################################
 binpData <- subset(input, solver == "BinP")
@@ -22,5 +31,5 @@ lowerBoundCosts <- subset(lowerBoundData, select = c(val))
 paste("avg LB: ", mean(lowerBoundCosts[["val"]]))
 ##############################################################################
 
-finalPlot <- plotPointsPre + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
+finalPlot <- scaledPlot + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
 ggsave(finalPlot, file="solver_instance_cost.png", width = 6, height = 4)
