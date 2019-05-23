49bbf59f319182a179186b175a0810858a5a3b4b
tbohne <tbohne@uos.de>
2019-05-23 23:09:48 +0200

    BipartiteGraph - refined doc / modifiers

--- StackingProblems/src/SP/representations/BipartiteGraph.java
+++ StackingProblems/src/SP/representations/BipartiteGraph.java
@@ -7,20 +7,22 @@ import java.util.Set;
 
 /**
  * Represents a bipartite graph consisting of two partitions.
+ * It is not checked whether the graph actually is bipartite, it's just a representation
+ * to store undirected graphs together with two partitions.
  *
  * @author Tim Bohne
  */
 public class BipartiteGraph {
 
-    private Set<String> partitionOne;
-    private Set<String> partitionTwo;
-    private DefaultUndirectedWeightedGraph<String, DefaultWeightedEdge> graph;
+    private final Set<String> partitionOne;
+    private final Set<String> partitionTwo;
+    private final DefaultUndirectedWeightedGraph<String, DefaultWeightedEdge> graph;
 
     /**
      * Constructor
      *
-     * @param partitionOne - the first partition of the bipartite graph
-     * @param partitionTwo - the second partition of the bipartite graph
+     * @param partitionOne - first partition of the bipartite graph
+     * @param partitionTwo - second partition of the bipartite graph
      * @param graph        - the actual graph
      */
     public BipartiteGraph(
@@ -36,7 +38,7 @@ public class BipartiteGraph {
     /**
      * Returns the bipartite graph's first partition.
      *
-     * @return the bipartite graph's first partition
+     * @return first partition of the graph
      */
     public Set<String> getPartitionOne() {
         return this.partitionOne;
@@ -45,7 +47,7 @@ public class BipartiteGraph {
     /**
      * Returns the bipartite graph's second partition.
      *
-     * @return the bipartite graph's second partition
+     * @return second partition of the graph
      */
     public Set<String> getPartitionTwo() {
         return this.partitionTwo;
@@ -54,7 +56,7 @@ public class BipartiteGraph {
     /**
      * Returns the bipartite graph.
      *
-     * @return the bipartite graph
+     * @return bipartite graph
      */
     public DefaultUndirectedWeightedGraph<String, DefaultWeightedEdge> getGraph() {
         return this.graph;
