4fb02de2c3c28268bc7c56ed03240acbab4f3d53
tbohne <tbohne@uos.de>
2019-05-25 15:23:14 +0200

    InstanceWriter - refined doc

--- StackingProblems/src/SP/io/InstanceWriter.java
+++ StackingProblems/src/SP/io/InstanceWriter.java
@@ -16,8 +16,8 @@ public class InstanceWriter {
     /**
      * Writes the stacking constraints to the file using the specified buffered writer.
      *
-     * @param instance - the instance that is written to the file
-     * @param bw       - the buffered writer pointing to the part of the file to write to
+     * @param instance - instance that is written to the file
+     * @param bw       - buffered writer pointing to the part of the file to write to
      * @throws IOException
      */
     private static void writeStackingConstraints(Instance instance, BufferedWriter bw) throws IOException {
@@ -33,8 +33,8 @@ public class InstanceWriter {
     /**
      * Writes the costs to the file using the specified buffered writer.
      *
-     * @param instance - the instance that is written to the file
-     * @param bw       - the buffered writer pointing to the part of the file to write to
+     * @param instance - instance that is written to the file
+     * @param bw       - buffered writer pointing to the part of the file to write to
      * @throws IOException
      */
     private static void writeCosts(Instance instance, BufferedWriter bw) throws IOException {
@@ -50,8 +50,8 @@ public class InstanceWriter {
     /**
      * Writes the item positions to the file using the specified buffered writer.
      *
-     * @param instance - the instance that is written to the file
-     * @param bw       - the buffered writer pointing to the part of the file to write to
+     * @param instance - instance that is written to the file
+     * @param bw       - buffered writer pointing to the part of the file to write to
      * @throws IOException
      */
     private static void writeItemPositions(Instance instance, BufferedWriter bw) throws IOException {
@@ -64,8 +64,8 @@ public class InstanceWriter {
     /**
      * Writes the stack positions to the file using the specified buffered writer.
      *
-     * @param instance - the instance that is written to the file
-     * @param bw       - the buffered writer pointing to the part of the file to write to
+     * @param instance - instance that is written to the file
+     * @param bw       - buffered writer pointing to the part of the file to write to
      * @throws IOException
      */
     private static void writeStackPositions(Instance instance, BufferedWriter bw) throws IOException {
@@ -75,6 +75,13 @@ public class InstanceWriter {
         bw.newLine();
     }
 
+    /**
+     * Writes the item dimensions to the file using the specified buffered writer.
+     *
+     * @param instance - instance that is written to the file
+     * @param bw       - buffered writer pointing to the part of the file to write to
+     * @throws IOException
+     */
     private static void writeItemDimensions(Instance instance, BufferedWriter bw) throws IOException {
         for (Item item : instance.getItemObjects()) {
             bw.write("(" + item.getLength() + "," + item.getWidth() + ")" + " ");
@@ -85,8 +92,8 @@ public class InstanceWriter {
     /**
      * Writes the specified instance to the file with the specified name.
      *
-     * @param filename - the file the instance gets written to
-     * @param instance - the instance to be written to the file
+     * @param filename - file the instance gets written to
+     * @param instance - instance to be written to the file
      */
     public static void writeInstance(String filename, Instance instance) {
 
@@ -120,22 +127,25 @@ public class InstanceWriter {
     /**
      * Writes the configuration of an instance set to the specified file.
      *
-     * @param filename                                     - the name of the file to be written to
-     * @param numOfInstances                               - the number of instances of the set
-     * @param numOfItems                                   - the number of items that are part of the instances
-     * @param stackCapacity                                - the capacity of the instance's stacks
-     * @param additionalStackPercentage                    - the percentage to be added upon the minimum number of stacks (n/b)
-     * @param chanceForOneInStackingConstraints            - the chance for a 1-entry in the stacking constraint matrix (not used atm)
-     * @param chanceForOneInPlacementConstraints           - the chance for a 1-entry in the placement constraint matrix
-     * @param usingStackingConstraintGenerationApproachOne - determines whether the 1st approach of stacking constraint generation is used
-     * @param itemLengthLB                                 - the lower bound of the item lengths
-     * @param itemLengthUB                                 - the upper bound of the item lengths
-     * @param itemWidthLB                                  - the lower bound of the item widths
-     * @param itemWidthUB                                  - the upper bound of the item widths
+     * @param filename                                     - name of the file to be written to
+     * @param numOfInstances                               - number of instances of the set
+     * @param numOfItems                                   - number of items that are part of the instances
+     * @param stackCapacity                                - capacity of the instance's stacks
+     * @param additionalStackPercentage                    - percentage to be added upon the minimum number of stacks (n/b)
+     * @param chanceForOneInStackingConstraints            - chance for a 1-entry in the stacking constraint matrix
+     * @param chanceForOneInPlacementConstraints           - chance for a 1-entry in the placement constraint matrix
+     * @param usingStackingConstraintGenerationApproachOne - determines whether the 1st approach of stacking constraint
+     *                                                       generation is used (2nd approach otherwise)
+     * @param itemLengthLB                                 - lower bound of the item lengths
+     * @param itemLengthUB                                 - upper bound of the item lengths
+     * @param itemWidthLB                                  - lower bound of the item widths
+     * @param itemWidthUB                                  - upper bound of the item widths
      */
-    public static void writeConfig(String filename, int numOfInstances, int numOfItems, int stackCapacity,
-        int additionalStackPercentage, float chanceForOneInStackingConstraints, float chanceForOneInPlacementConstraints,
-        boolean usingStackingConstraintGenerationApproachOne, float itemLengthLB, float itemLengthUB, float itemWidthLB, float itemWidthUB
+    public static void writeConfig(
+        String filename, int numOfInstances, int numOfItems, int stackCapacity, int additionalStackPercentage,
+        float chanceForOneInStackingConstraints, float chanceForOneInPlacementConstraints,
+        boolean usingStackingConstraintGenerationApproachOne, float itemLengthLB, float itemLengthUB, float itemWidthLB,
+        float itemWidthUB
     ) {
 
         File file = new File(filename);
