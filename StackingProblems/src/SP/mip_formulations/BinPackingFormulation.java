8ffe93815acf9d321bfd73ce5254557a1246da57
tbohne <tbohne@uos.de>
2019-05-25 20:19:06 +0200

    BinP - refined doc / minor changes

--- StackingProblems/src/SP/mip_formulations/BinPackingFormulation.java
+++ StackingProblems/src/SP/mip_formulations/BinPackingFormulation.java
@@ -9,17 +9,17 @@ import ilog.concert.IloLinearNumExpr;
 import ilog.cplex.IloCplex;
 
 /**
- * Represents the bin-packing MIP formulation of stacking problems that is solved by CPLEX.
+ * Represents the bin-packing MIP formulation of stacking problems that gets solved by CPLEX.
  *
  * @author Tim Bohne
  */
 public class BinPackingFormulation {
 
-    private Instance instance;
-    private double timeLimit;
-    private boolean hideCPLEXOutput;
-    private int mipEmphasis;
-    private double tolerance;
+    private final Instance instance;
+    private final double timeLimit;
+    private final boolean hideCPLEXOutput;
+    private final int mipEmphasis;
+    private final double tolerance;
 
     /**
      * Constructor
@@ -39,7 +39,7 @@ public class BinPackingFormulation {
     }
 
     /**
-     * Solves the stacking problem using the bin-packing formulation.
+     * Solves the instance of a stacking problem using the bin-packing formulation.
      *
      * @return generated solution to the stacking problem
      */
@@ -92,7 +92,7 @@ public class BinPackingFormulation {
     }
 
     /**
-     * Initializes the variables x_iq.
+     * Initializes the variables x_iq (x_iq = 1 --> item i gets placed in stack q).
      *
      * @param cplex - CPLEX model
      * @param x     - variables x_iq
@@ -101,6 +101,51 @@ public class BinPackingFormulation {
     private void initVariables(IloCplex cplex, IloIntVar[][] x) throws ilog.concert.IloException {
         for (int i = 0; i < this.instance.getItems().length; i++) {
             x[i] = cplex.intVarArray(this.instance.getStacks().length, 0, 1);
+        }
+    }
+
+    /**
+     * Adds the bin-packing formulation's constraints to the CPLEX model.
+     *
+     * @param cplex - CPLEX model
+     * @param x     - variables x_iq
+     * @throws ilog.concert.IloException for CPLEX errors
+     */
+    private void addConstraints(IloCplex cplex, IloIntVar[][] x) throws ilog.concert.IloException {
+
+        // --- Constraint (2) ---
+        for (int i = 0; i < this.instance.getItems().length; i++) {
+            IloLinearIntExpr expr = cplex.linearIntExpr();
+            for (int q = 0; q < this.instance.getStacks().length; q++) {
+                if (this.instance.getPlacementConstraints()[i][q] == 1) {
+                    expr.addTerm(1, x[i][q]);
+                }
+            }
+            cplex.addEq(expr, 1);
+        }
+
+        // --- Constraint (3) ---
+        for (int q = 0; q < this.instance.getStacks().length; q++) {
+            IloLinearIntExpr expr = cplex.linearIntExpr();
+            for (int i = 0; i < this.instance.getItems().length; i++) {
+                expr.addTerm(1, x[i][q]);
+            }
+            cplex.addLe(expr, this.instance.getStackCapacity());
+        }
+
+        // --- Constraint (4) ---
+        for (int i = 0; i < this.instance.getItems().length; i++) {
+            for (int j = 0; j < this.instance.getItems().length; j++) {
+
+                if (i != j && this.instance.getStackingConstraints()[i][j] == 0 && this.instance.getStackingConstraints()[j][i] == 0) {
+                    for (int q = 0; q < this.instance.getStacks().length; q++) {
+                        IloLinearIntExpr expr = cplex.linearIntExpr();
+                        expr.addTerm(1, x[i][q]);
+                        expr.addTerm(1, x[j][q]);
+                        cplex.addLe(expr, 1);
+                    }
+                }
+            }
         }
     }
 
@@ -116,55 +161,8 @@ public class BinPackingFormulation {
             for (int q = 0; q < x[0].length; q++) {
                 if (Math.round(cplex.getValue(x[i][q])) == 1) {
                     int idx = 0;
-                    while (this.instance.getStacks()[q][idx] != -1) {
-                        idx++;
-                    }
+                    while (this.instance.getStacks()[q][idx] != -1) { idx++; }
                     this.instance.getStacks()[q][idx] = i;
-                }
-            }
-        }
-    }
-
-    /**
-     * Adds the bin-packing formulation's constraints to the CPLEX model.
-     *
-     * @param cplex - CPLEX model
-     * @param x     - variables x_iq
-     * @throws ilog.concert.IloException for CPLEX errors
-     */
-    private void addConstraints(IloCplex cplex, IloIntVar[][] x) throws ilog.concert.IloException {
-
-        // --- (2) ---
-        for (int i = 0; i < this.instance.getItems().length; i++) {
-            IloLinearIntExpr expr = cplex.linearIntExpr();
-            for (int q = 0; q < this.instance.getStacks().length; q++) {
-                if (this.instance.getPlacementConstraints()[i][q] == 1) {
-                    expr.addTerm(1, x[i][q]);
-                }
-            }
-            cplex.addEq(expr, 1);
-        }
-
-        // --- (3) ---
-        for (int q = 0; q < this.instance.getStacks().length; q++) {
-            IloLinearIntExpr expr = cplex.linearIntExpr();
-            for (int i = 0; i < this.instance.getItems().length; i++) {
-                expr.addTerm(1, x[i][q]);
-            }
-            cplex.addLe(expr, this.instance.getStackCapacity());
-        }
-
-        // --- (4) ---
-        for (int i = 0; i < this.instance.getItems().length; i++) {
-            for (int j = 0; j < this.instance.getItems().length; j++) {
-
-                if (i != j && this.instance.getStackingConstraints()[i][j] == 0 && this.instance.getStackingConstraints()[j][i] == 0) {
-                    for (int q = 0; q < this.instance.getStacks().length; q++) {
-                        IloLinearIntExpr expr = cplex.linearIntExpr();
-                        expr.addTerm(1, x[i][q]);
-                        expr.addTerm(1, x[j][q]);
-                        cplex.addLe(expr, 1);
-                    }
                 }
             }
         }
@@ -185,7 +183,7 @@ public class BinPackingFormulation {
         // set time limit
         cplex.setParam(IloCplex.Param.TimeLimit, this.timeLimit);
 
-        // controls trade-offs between speed, feasibility, optimality,
+        // control trade-offs between speed, feasibility and optimality
         cplex.setParam(IloCplex.IntParam.MIPEmphasis, this.mipEmphasis);
 
         // set termination tolerance
