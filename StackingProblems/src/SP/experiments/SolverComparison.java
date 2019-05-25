b3cc08fa7291d32909d35912197b47105783e32e
tbohne <tbohne@uos.de>
2019-05-26 00:19:12 +0200

    SolverComparison - refactoring / cleanin up / adding doc

--- StackingProblems/src/SP/experiments/SolverComparison.java
+++ StackingProblems/src/SP/experiments/SolverComparison.java
@@ -14,33 +14,49 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Provides functionalities for comparisons of different solvers for stacking problems.
+ *
+ * @author Tim Bohne
+ */
 public class SolverComparison {
 
-    protected String solutionPrefix;
-    protected String instancePrefix;
-    protected double timeLimit;
+    protected final String solutionPrefix;
+    protected final String instancePrefix;
+    protected final double timeLimit;
 
-    protected boolean hideCPLEXOutput;
-    protected int mipEmphasis;
-    protected double tolerance;
+    // cplex config
+    private final boolean hideCPLEXOutput;
+    private final int mipEmphasis;
+    private final double mipTolerance;
 
-
+    /**
+     * Constructor
+     *
+     * @param solutionPrefix  - prefix for solutions
+     * @param instancePrefix  - prefix for instances
+     * @param timeLimit       - time limit for solving procedure
+     * @param hideCPLEXOutput - determines whether cplex output should be hidden
+     * @param mipEmphasis     - control trade-offs between speed, feasibility and optimality
+     * @param mipTolerance    - termination tolerance for cplex (MIPs)
+     */
     public SolverComparison(
-        String solutionPrefix, String instancePrefix, double timeLimit, boolean hideCPLEXOutput, int mipEmphasis, double tolerance
+        String solutionPrefix, String instancePrefix, double timeLimit,
+        boolean hideCPLEXOutput, int mipEmphasis, double mipTolerance
     ) {
         this.solutionPrefix = solutionPrefix;
         this.instancePrefix = instancePrefix;
         this.timeLimit = timeLimit;
         this.hideCPLEXOutput = hideCPLEXOutput;
         this.mipEmphasis = mipEmphasis;
-        this.tolerance = tolerance;
+        this.mipTolerance = mipTolerance;
     }
 
     /**
      * Computes a lower bound for the specified instance using the LowerBoundsCalculator.
      *
-     * @param instance     - the instance to compute a LB for
-     * @param solutionName - the name of the generated solution
+     * @param instance     - instance to compute a LB for
+     * @param solutionName - name of the generated solution
      */
     public void computeLowerBound(Instance instance, String solutionName) {
         LowerBoundsCalculator lbc = new LowerBoundsCalculator(instance);
@@ -52,38 +68,46 @@ public class SolverComparison {
     /**
      * Solves the given instance with the bin-packing solver.
      *
-     * @param instance     - the instance to be solved
-     * @param solutionName - the name of the generated solution
+     * @param instance     - instance to be solved
+     * @param solutionName - name of the generated solution
      */
     public void solveWithBinPacking(Instance instance, String solutionName) {
         BinPackingFormulation binPackingFormulation = new BinPackingFormulation(
-                instance, this.timeLimit, this.hideCPLEXOutput, this.mipEmphasis, this.tolerance
+            instance, this.timeLimit, this.hideCPLEXOutput, this.mipEmphasis, this.mipTolerance
         );
         Solution sol = binPackingFormulation.solve();
-        SolutionWriter.writeSolution(this.solutionPrefix + solutionName + ".txt", sol, RepresentationUtil.getNameOfSolver(Solvers.Solver.MIP_BINPACKING));
-        SolutionWriter.writeSolutionAsCSV(this.solutionPrefix + "solutions.csv", sol, RepresentationUtil.getAbbreviatedNameOfSolver(Solvers.Solver.MIP_BINPACKING));
+        SolutionWriter.writeSolution(
+            this.solutionPrefix + solutionName + ".txt", sol, RepresentationUtil.getNameOfSolver(Solvers.Solver.MIP_BINPACKING)
+        );
+        SolutionWriter.writeSolutionAsCSV(
+            this.solutionPrefix + "solutions.csv", sol, RepresentationUtil.getAbbreviatedNameOfSolver(Solvers.Solver.MIP_BINPACKING)
+        );
     }
 
     /**
      * Solves the given instance with the three-index solver.
      *
-     * @param instance     - the instance to be solved
-     * @param solutionName - the name of the generated solution
+     * @param instance     - instance to be solved
+     * @param solutionName - name of the generated solution
      */
     public void solveWithThreeIdx(Instance instance, String solutionName) {
         ThreeIndexFormulation threeIndexFormulation = new ThreeIndexFormulation(
-                instance, this.timeLimit, this.hideCPLEXOutput, this.mipEmphasis, this.tolerance
+            instance, this.timeLimit, this.hideCPLEXOutput, this.mipEmphasis, this.mipTolerance
         );
         Solution sol = threeIndexFormulation.solve();
-        SolutionWriter.writeSolution(this.solutionPrefix + solutionName + ".txt", sol, RepresentationUtil.getNameOfSolver(Solvers.Solver.MIP_THREEINDEX));
-        SolutionWriter.writeSolutionAsCSV(this.solutionPrefix + "solutions.csv", sol, RepresentationUtil.getAbbreviatedNameOfSolver(Solvers.Solver.MIP_THREEINDEX));
+        SolutionWriter.writeSolution(
+            this.solutionPrefix + solutionName + ".txt", sol, RepresentationUtil.getNameOfSolver(Solvers.Solver.MIP_THREEINDEX)
+        );
+        SolutionWriter.writeSolutionAsCSV(
+            this.solutionPrefix + "solutions.csv", sol, RepresentationUtil.getAbbreviatedNameOfSolver(Solvers.Solver.MIP_THREEINDEX)
+        );
     }
 
     /**
      * Workaround that triggers the ClassLoader to load all classes that are needed during the solving procedure.
      * Otherwise the first instance to be solved would have a longer runtime because of the initial class loading.
      *
-     * @param instanceFile - the file to load the instance from
+     * @param instanceFile - file to load the instance from
      */
     public void prepareRuntimeMeasurementByPreLoadingAllClasses(File instanceFile) {
         String instanceName = instanceFile.toString().replace("res/instances/", "").replace(".txt", "");
@@ -93,37 +117,38 @@ public class SolverComparison {
     }
 
     /**
-     * Compares the different solvers for a stack capacity of 2.
+     * Computes lower bounds and compares the bin-packing and three-index formulation.
+     * The results are written to the file system.
      *
      * @param solversToBeCompared - determines the solvers that are supposed to be compared
      */
     public void compareSolvers(List<Solvers.Solver> solversToBeCompared) {
+
         File dir = new File(this.instancePrefix);
         File[] directoryListing = dir.listFiles();
+        assert directoryListing != null;
         Arrays.sort(directoryListing);
         String allSol = HeuristicUtil.createStringContainingAllSolutionNames(this.solutionPrefix);
 
-        if (directoryListing != null) {
-            for (File file : directoryListing) {
+        for (File file : directoryListing) {
 
-                if (file.toString().contains("slp_instance_") && !allSol.contains(file.toString().replace("res/instances/", ""))) {
-                    String instanceName = file.toString().replace("res/instances/", "").replace(".txt", "");
-                    System.out.println("working on: " + instanceName);
-                    Instance instance = InstanceReader.readInstance(this.instancePrefix + instanceName + ".txt");
-                    String solutionName = instanceName.replace("instance", "sol");
+            if (file.toString().contains("slp_instance_") && !allSol.contains(file.toString().replace("res/instances/", ""))) {
+                String instanceName = file.toString().replace("res/instances/", "").replace(".txt", "");
+                System.out.println("working on: " + instanceName);
+                Instance instance = InstanceReader.readInstance(this.instancePrefix + instanceName + ".txt");
+                String solutionName = instanceName.replace("instance", "sol");
 
-                    computeLowerBound(instance, solutionName);
+                computeLowerBound(instance, solutionName);
 
-                    if (solversToBeCompared.contains(Solvers.Solver.MIP_BINPACKING)) {
-                        System.out.println("solve with binpacking");
-                        solveWithBinPacking(instance, solutionName);
-                        instance.resetStacks();
-                    }
-                    if (solversToBeCompared.contains(Solvers.Solver.MIP_THREEINDEX)) {
-                        System.out.println("solve with 3idx");
-                        solveWithThreeIdx(instance, solutionName);
-                        instance.resetStacks();
-                    }
+                if (solversToBeCompared.contains(Solvers.Solver.MIP_BINPACKING)) {
+                    System.out.println("solve with binpacking");
+                    solveWithBinPacking(instance, solutionName);
+                    instance.resetStacks();
+                }
+                if (solversToBeCompared.contains(Solvers.Solver.MIP_THREEINDEX)) {
+                    System.out.println("solve with 3idx");
+                    solveWithThreeIdx(instance, solutionName);
+                    instance.resetStacks();
                 }
             }
         }
