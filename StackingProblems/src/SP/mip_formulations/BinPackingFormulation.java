1b6acf59ffa1b4f08b9034562ff8f6c36055f52e
tbohne <tbohne@uos.de>
2019-05-25 19:44:25 +0200

    BinP - refactoring / replaced inefficient method to sort items in stacks

--- StackingProblems/src/SP/mip_formulations/BinPackingFormulation.java
+++ StackingProblems/src/SP/mip_formulations/BinPackingFormulation.java
@@ -9,7 +9,7 @@ import ilog.concert.IloLinearNumExpr;
 import ilog.cplex.IloCplex;
 
 /**
- * Represents the bin-packing formulation (MIP) of stacking problems to be used with CPLEX.
+ * Represents the bin-packing MIP formulation of stacking problems that is solved by CPLEX.
  *
  * @author Tim Bohne
  */
@@ -21,8 +21,8 @@ public class BinPackingFormulation {
     /**
      * Constructor
      *
-     * @param instance  - the instance to be solved
-     * @param timeLimit - the time limit for the solving procedure
+     * @param instance  - instance to be solved
+     * @param timeLimit - time limit for the solving procedure
      */
     public BinPackingFormulation(Instance instance, double timeLimit) {
         this.instance = instance;
@@ -30,95 +30,104 @@ public class BinPackingFormulation {
     }
 
     /**
+     * Adds the bin-packing formulation's constraints to the CPLEX model.
+     *
+     * @param cplex - CPLEX model
+     * @param x     - variables x_iq
+     * @throws ilog.concert.IloException for CPLEX errors
+     */
+    private void addConstraints(IloCplex cplex, IloIntVar[][] x) throws ilog.concert.IloException {
+
+        // --- (2) ---
+        for (int i = 0; i < this.instance.getItems().length; i++) {
+            IloLinearIntExpr expr = cplex.linearIntExpr();
+            for (int q = 0; q < this.instance.getStacks().length; q++) {
+                if (this.instance.getPlacementConstraints()[i][q] == 1) {
+                    expr.addTerm(1, x[i][q]);
+                }
+            }
+            cplex.addEq(expr, 1);
+        }
+
+        // --- (3) ---
+        for (int q = 0; q < this.instance.getStacks().length; q++) {
+            IloLinearIntExpr expr = cplex.linearIntExpr();
+            for (int i = 0; i < this.instance.getItems().length; i++) {
+                expr.addTerm(1, x[i][q]);
+            }
+            cplex.addLe(expr, this.instance.getStackCapacity());
+        }
+
+        // --- (4) ---
+        for (int i = 0; i < this.instance.getItems().length; i++) {
+            for (int j = 0; j < this.instance.getItems().length; j++) {
+
+                if (i != j && this.instance.getStackingConstraints()[i][j] == 0 && this.instance.getStackingConstraints()[j][i] == 0) {
+                    for (int q = 0; q < this.instance.getStacks().length; q++) {
+                        IloLinearIntExpr expr = cplex.linearIntExpr();
+                        expr.addTerm(1, x[i][q]);
+                        expr.addTerm(1, x[j][q]);
+                        cplex.addLe(expr, 1);
+                    }
+                }
+            }
+        }
+    }
+
+    private void setCPLEXConfig(IloCplex cplex) throws ilog.concert.IloException {
+        // set CPLEX output
+        cplex.setOut(null);
+
+        // set time limit
+        cplex.setParam(IloCplex.Param.TimeLimit, this.timeLimit);
+
+        // 1 --> emphasize feasibility over optimality
+        cplex.setParam(IloCplex.IntParam.MIPEmphasis, 1);
+
+        // set tolerance to 0.0 - CPLEX will only terminate before the time limit if the actual optimum is found
+        cplex.setParam(IloCplex.DoubleParam.EpAGap, 0.0);
+        cplex.setParam(IloCplex.DoubleParam.EpGap, 0.0);
+    }
+
+    /**
      * Solves the stacking problem using the bin-packing formulation.
      *
-     * @return the generated solution to the stacking problem
+     * @return generated solution to the stacking problem
      */
     public Solution solve() {
 
         Solution sol = new Solution();
 
         try {
-
-            // Defines a new model.
+            // define new model
             IloCplex cplex = new IloCplex();
 
-            // VARIABLES
-
+            // init variables x_iq
             IloIntVar[][] x = new IloIntVar[this.instance.getItems().length][];
-
             for (int i = 0; i < this.instance.getItems().length; i++) {
                 x[i] = cplex.intVarArray(this.instance.getStacks().length, 0, 1);
             }
 
-            // OBJECTIVE FUNCTION
-
+            // define objective
             IloLinearNumExpr objective = cplex.linearNumExpr();
-
             for (int i = 0; i < this.instance.getItems().length; i++) {
                 for (int q = 0; q < this.instance.getStacks().length; q++) {
                     objective.addTerm(this.instance.getCosts()[i][q], x[i][q]);
                 }
             }
-
             cplex.addMinimize(objective);
 
-            // CONSTRAINTS
+            this.addConstraints(cplex, x);
 
-            // --- (2) ---
-            for (int i = 0; i < this.instance.getItems().length; i++) {
-                IloLinearIntExpr expr = cplex.linearIntExpr();
-                for (int q = 0; q < this.instance.getStacks().length; q++) {
-                    if (this.instance.getPlacementConstraints()[i][q] == 1) {
-                        expr.addTerm(1, x[i][q]);
-                    }
-                }
-                cplex.addEq(expr, 1);
-            }
-
-            // --- (3) ---
-            for (int q = 0; q < this.instance.getStacks().length; q++) {
-                IloLinearIntExpr expr = cplex.linearIntExpr();
-                for (int i = 0; i < this.instance.getItems().length; i++) {
-                    expr.addTerm(1, x[i][q]);
-                }
-                cplex.addLe(expr, this.instance.getStackCapacity());
-            }
-
-            // --- (4) ---
-            for (int i = 0; i < this.instance.getItems().length; i++) {
-                for (int j = 0; j < this.instance.getItems().length; j++) {
-
-                    if (i != j && this.instance.getStackingConstraints()[i][j] == 0 && this.instance.getStackingConstraints()[j][i] == 0) {
-                        for (int q = 0; q < this.instance.getStacks().length; q++) {
-                            IloLinearIntExpr expr = cplex.linearIntExpr();
-                            expr.addTerm(1, x[i][q]);
-                            expr.addTerm(1, x[j][q]);
-                            cplex.addLe(expr, 1);
-                        }
-                    }
-                }
-            }
-
-            System.out.println();
-            cplex.setOut(null);
-
-            // sets the time limit
-            cplex.setParam(IloCplex.Param.TimeLimit, timeLimit);
-
+            this.setCPLEXConfig(cplex);
             double startTime = cplex.getCplexTime();
-            // 1 --> emphasizes feasibility over optimality
-            cplex.setParam(IloCplex.IntParam.MIPEmphasis, 1);
-            // sets the tolerance to 0.0 - CPLEX will only terminate before the time limit if the actual optimum is found
-            cplex.setParam(IloCplex.DoubleParam.EpAGap, 0.0);
-            cplex.setParam(IloCplex.DoubleParam.EpGap, 0.0);
 
             if (cplex.solve()) {
                 double timeToSolve = cplex.getCplexTime() - startTime;
                 this.setStacks(cplex, x);
-                this.getSolutionFromStackAssignment();
+                this.instance.lowerItemsThatAreStackedInTheAir();
                 sol = new Solution(timeToSolve, timeLimit, this.instance);
-                sol.lowerItemsThatAreStackedInTheAir();
+                sol.sortItemsInStacksBasedOnTransitiveStackingConstraints();
             }
             cplex.end();
 
@@ -126,36 +135,6 @@ public class BinPackingFormulation {
             e.printStackTrace();
         }
         return sol;
-    }
-
-    /**
-     * Extracts a feasible solution that respects the stacking constraints from the generated stack assignments.
-     * It is guaranteed that such a solution is obtainable from the generated assignments.
-     */
-    public void getSolutionFromStackAssignment() {
-
-        for (int stack = 0; stack < this.instance.getStacks().length; stack++) {
-
-            boolean somethingChanged = true;
-
-            while (somethingChanged) {
-
-                somethingChanged = false;
-
-                for (int item = 0; item < this.instance.getStackCapacity() - 1; item++) {
-
-                    if (this.instance.getStacks()[stack][item] != -1 && this.instance.getStacks()[stack][item + 1] != -1) {
-                        if (this.instance.getStackingConstraints()[this.instance.getStacks()[stack][item]][this.instance.getStacks()[stack][item + 1]] == 0) {
-                            int tmp = this.instance.getStacks()[stack][item];
-                            this.instance.getStacks()[stack][item] = this.instance.getStacks()[stack][item + 1];
-                            this.instance.getStacks()[stack][item + 1] = tmp;
-                            somethingChanged = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
     }
 
     /**
