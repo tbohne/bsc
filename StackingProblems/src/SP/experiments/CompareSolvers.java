6d29918027db7bf502567d062b6e7eed3c51ac63
tbohne <tbohne@uos.de>
2019-05-26 14:04:49 +0200

    CompareSolvers - refined config of solver comparison

--- StackingProblems/src/SP/experiments/CompareSolvers.java
+++ StackingProblems/src/SP/experiments/CompareSolvers.java
@@ -1,56 +1,87 @@
 package SP.experiments;
 
 import SP.representations.Solvers;
+import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Used to start configure / start the solver comparison.
+ * Used to configure / start the solver comparison.
  *
  * @author Tim Bohne
  */
 public class CompareSolvers {
 
-    public static String INSTANCE_PREFIX = "res/instances/";
-    public static String SOLUTION_PREFIX = "res/solutions/";
+    private static final String INSTANCE_PREFIX = "res/instances/";
+    private static final String SOLUTION_PREFIX = "res/solutions/";
 
-    /************ CPLEX CONFIG ************/
-    public static boolean HIDE_CPLEX_OUTPUT = true;
+    // solvers to be used
+    private static final List<Solvers.Solver> SOLVERS = Lists.newArrayList(
+        Solvers.Solver.MIP_BINPACKING,
+        Solvers.Solver.MIP_THREEINDEX,
+        Solvers.Solver.CONSTRUCTIVE_TWO_CAP,
+        Solvers.Solver.CONSTRUCTIVE_THREE_CAP
+    );
+
+    /********************** CPLEX CONFIG **********************/
+    private static final boolean HIDE_CPLEX_OUTPUT = true;
     // 1 --> feasibility over optimality
-    public static int MIP_EMPHASIS = 1;
+    private static final int MIP_EMPHASIS = 1;
     // 0 tolerance --> only terminating with optimal solutions before time limit
-    public static double MIP_TOLERANCE = 0.0;
-    /**************************************/
+    private static final double MIP_TOLERANCE = 0.0;
+    /**********************************************************/
 
-    // Specifies the time limit for the solving procedure in seconds.
-    public static double TIME_LIMIT = 300;
+    // specifies the time limit for the solving procedure in seconds
+    private static final double TIME_LIMIT = 300;
 
-    // The 3Cap heuristic has an option to prioritize the runtime which can be set here.
-    public static boolean PRIORITIZE_RUNTIME = false;
-    // 2Cap and 3Cap provide post processing procedures that can be enabled here.
-    public static boolean POST_PROCESSING = true;
+    // 2Cap and 3Cap provide post processing procedures that can be enabled here
+    private static final boolean POST_PROCESSING = true;
 
-    public static int THRESHOLD_LB = 20;
-    public static int THRESHOLD_UB = 75;
-    public static int STEP_SIZE = 5;
-    public static float SPLIT_PAIRS_DIVISOR = 3.4F;
-    public static float PENALTY_FACTOR = 5.0F;
+    /********************** 3CAP CONFIG **********************/
+    // 3Cap has an option to prioritize the runtime which can be set here
+    private static final boolean PRIORITIZE_RUNTIME = false;
+    // rating system config
+    private static final int THRESHOLD_LB = 20;
+    private static final int THRESHOLD_UB = 75;
+    private static final int STEP_SIZE = 5;
+    private static final float SPLIT_PAIRS_DIVISOR = 3.4F;
+    private static final float PENALTY_FACTOR = 5.0F;
+    /*********************************************************/
+
+    // determines whether only b=2 or b=3 instances are considered (general b otherwise)
+    private static final boolean SOLVE_INSTANCES_WITH_SPECIFIC_CAPACITY = true;
+    // in case of specific capacity: determines whether only b=2 instances are considered (b=3 otherwise)
+    private static final boolean SOLVE_TWO_CAP = true;
 
     public static void main(String[] args) {
 
-        ArrayList<Solvers.Solver> solversToBeUsed = new ArrayList<>();
-//        solversToBeUsed.add(SolverComparison.Solver.MIP_BINPACKING);
-//        solversToBeUsed.add(SolverComparison.Solver.MIP_THREEINDEX);
-//        solversToBeUsed.add(SolverComparison.Solver.CONSTRUCTIVE_TWO_CAP);
-        solversToBeUsed.add(Solvers.Solver.CONSTRUCTIVE_THREE_CAP);
+        if (SOLVE_INSTANCES_WITH_SPECIFIC_CAPACITY) {
 
-//         SolverComparison2Cap twoCap = new SolverComparison2Cap();
-//         twoCap.compareSolvers(solversToBeUsed);
+            // solving instances with capacity 2
+            if (SOLVE_TWO_CAP) {
 
-        SolverComparison3Cap threeCap = new SolverComparison3Cap(
-            SOLUTION_PREFIX, INSTANCE_PREFIX, TIME_LIMIT, HIDE_CPLEX_OUTPUT, MIP_EMPHASIS, MIP_TOLERANCE,
-            POST_PROCESSING, PRIORITIZE_RUNTIME, THRESHOLD_LB, THRESHOLD_UB, STEP_SIZE, SPLIT_PAIRS_DIVISOR, PENALTY_FACTOR
-        );
-       threeCap.compareSolvers(solversToBeUsed);
+                SolverComparison2Cap twoCap = new SolverComparison2Cap(
+                    SOLUTION_PREFIX, INSTANCE_PREFIX, TIME_LIMIT, HIDE_CPLEX_OUTPUT,
+                    MIP_EMPHASIS, MIP_TOLERANCE, POST_PROCESSING
+                );
+                twoCap.compareSolvers(SOLVERS);
+
+            // solving instances with capacity 3
+            } else {
+                SolverComparison3Cap threeCap = new SolverComparison3Cap(
+                    SOLUTION_PREFIX, INSTANCE_PREFIX, TIME_LIMIT, HIDE_CPLEX_OUTPUT,
+                    MIP_EMPHASIS, MIP_TOLERANCE, POST_PROCESSING, PRIORITIZE_RUNTIME,
+                    THRESHOLD_LB, THRESHOLD_UB, STEP_SIZE, SPLIT_PAIRS_DIVISOR, PENALTY_FACTOR
+                );
+                threeCap.compareSolvers(SOLVERS);
+            }
+
+        // only using solvers for general b
+        } else {
+            SolverComparison solverComp = new SolverComparison(
+                SOLUTION_PREFIX, INSTANCE_PREFIX, TIME_LIMIT, HIDE_CPLEX_OUTPUT, MIP_EMPHASIS, MIP_TOLERANCE
+            );
+            solverComp.compareSolvers(SOLVERS);
+        }
     }
 }
