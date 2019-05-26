344a35e29fd4bfeec21161431f9d905922db8b85
tbohne <tbohne@uos.de>
2019-05-26 21:13:18 +0200

    added dir for post optimization scripts

--- StackingProblems/src/SP/scripts/post_optimization_scipts/post_optimization_costs.R
+++ StackingProblems/src/SP/scripts/post_optimization_scipts/post_optimization_costs.R
@@ -4,7 +4,7 @@ input <- read.csv(file = "../../../res/solutions/solutions_imp.csv", header = TR
 solverEntries <- subset(input, solver == "2Cap + TS" | solver == "3Cap + TS" | solver == "OPT" | solver == "2Cap" | solver == "3Cap")
 
 plotPointsPre <- ggplot(data = solverEntries, aes(x = val, y = instance, color = solver, group = solver))
-scaledPlot <- plotPointsPre + geom_point() + xlab("costs") + ylab("instance") #+ scale_x_continuous(limits = c(6500, 6750))
+scaledPlot <- plotPointsPre + geom_point() + xlab("costs") + ylab("instance")
 finalPlot <- scaledPlot + scale_color_manual(values=c("#fa9f27", "#5428ff", "#f5503b", "#28bd5a"))
 
 ggsave(finalPlot, file = "solver_instance_cost.png", width=6, height=4)
