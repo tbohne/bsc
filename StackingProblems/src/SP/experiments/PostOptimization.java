597f4e9de24a2c02f0f933a6092327183547bfb7
tbohne <tbohne@uos.de>
2019-05-26 19:04:30 +0200

    PostOptimization - implemented TS config

--- StackingProblems/src/SP/experiments/PostOptimization.java
+++ StackingProblems/src/SP/experiments/PostOptimization.java
@@ -10,31 +10,75 @@ import SP.util.RepresentationUtil;
 
 import java.util.List;
 
+/**
+ * Applies a configurable post optimization method to previously generated solutions for stacking problems.
+ *
+ * @author Tim Bohne
+ */
 public class PostOptimization {
+
+    /**
+     * Enumeration containing the different stopping criteria for the tabu search.
+     */
+    public enum StoppingCriteria {
+        ITERATIONS,
+        TABU_LIST_CLEARS,
+        NON_IMPROVING_ITERATIONS
+    }
+
+    /**
+     * Enumeration containing the different short term strategies for the tabu search.
+     */
+    public enum ShortTermStrategies {
+        FIRST_FIT,
+        BEST_FIT
+    }
+
+    public static final ShortTermStrategies SHORT_TERM_STRATEGY = ShortTermStrategies.BEST_FIT;
+    public static final StoppingCriteria STOPPING_CRITERION = StoppingCriteria.NON_IMPROVING_ITERATIONS;
+
+    public static final int XSWAP_INTERVAL_UB = 4;
+
+    public static final int NUMBER_OF_ITERATIONS = 50;
+    public static final int NUMBER_OF_TABU_LIST_CLEARS = 10;
+    public static final int NUMBER_OF_NON_IMPROVING_ITERATIONS = 50;
+
+    public static final int NUMBER_OF_NEIGHBORS = 1000;
+    public static final int MAX_TABU_LIST_LENGTH_FACTOR = 9;
+    public static final int UNSUCCESSFUL_NEIGHBOR_GENERATION_ATTEMPTS = 5000;
+    public static final int UNSUCCESSFUL_X_SWAP_ATTEMPTS = 1000;
 
     public static final String INSTANCE_PREFIX = "res/instances/";
     public static final String SOLUTION_PREFIX = "res/solutions/";
 
-    public static final String SOLVER = "ThreeCapHeuristic";
-//    public static final String SOLVER = "TwoCapHeuristic";
+    // 3Cap solutions otherwise
+    public static final boolean OPTIMIZE_TWO_CAP_SOLUTIONS = true;
 
     public static void optimizeSolutions() {
 
-        List<OptimizableSolution> solutions = SolutionReader.readSolutionsFromDir(SOLUTION_PREFIX, INSTANCE_PREFIX, SOLVER);
+        List<OptimizableSolution> solutions;
+        if (OPTIMIZE_TWO_CAP_SOLUTIONS) {
+            solutions = SolutionReader.readSolutionsFromDir(
+                SOLUTION_PREFIX, INSTANCE_PREFIX, RepresentationUtil.getNameOfSolver(Solvers.Solver.CONSTRUCTIVE_TWO_CAP)
+            );
+        } else {
+            solutions = SolutionReader.readSolutionsFromDir(
+                SOLUTION_PREFIX, INSTANCE_PREFIX, RepresentationUtil.getNameOfSolver(Solvers.Solver.CONSTRUCTIVE_THREE_CAP)
+            );
+        }
 
         for (OptimizableSolution sol : solutions) {
 
             double startTime = System.currentTimeMillis();
-            double costsBefore = sol.getSol().computeCosts();
 
-            TabuSearch ts = new TabuSearch(sol.getSol(), sol.getOptimalObjectiveValue());
+            TabuSearch ts = new TabuSearch(
+                sol.getSol(), sol.getOptimalObjectiveValue(), NUMBER_OF_NEIGHBORS, MAX_TABU_LIST_LENGTH_FACTOR, SHORT_TERM_STRATEGY,
+                    UNSUCCESSFUL_NEIGHBOR_GENERATION_ATTEMPTS, UNSUCCESSFUL_X_SWAP_ATTEMPTS, NUMBER_OF_NON_IMPROVING_ITERATIONS,
+                    XSWAP_INTERVAL_UB, NUMBER_OF_ITERATIONS, NUMBER_OF_TABU_LIST_CLEARS, STOPPING_CRITERION
+            );
+
             Solution impSol = ts.solve();
             impSol.setTimeToSolve((System.currentTimeMillis() - startTime) / 1000.0);
-
-            System.out.println("costs before: " + costsBefore);
-            System.out.println("costs after: " + impSol.computeCosts());
-            System.out.println("total difference: " + (costsBefore - impSol.computeCosts()));
-            System.out.println("runtime: " + impSol.getTimeToSolve());
 
             SolutionWriter.writeSolution(
                 SOLUTION_PREFIX
@@ -48,7 +92,7 @@ public class PostOptimization {
             } else {
                 solver = Solvers.Solver.CONSTRUCTIVE_THREE_CAP;
             }
-            SolutionWriter.writeSolutionAsCSV(SOLUTION_PREFIX + "solutions_imp.csv", sol.getSol(), RepresentationUtil.getNameOfSolver(solver));
+            SolutionWriter.writeSolutionAsCSV(SOLUTION_PREFIX + "solutions_imp.csv", sol.getSol(), RepresentationUtil.getAbbreviatedNameOfSolver(solver));
             SolutionWriter.writeOptAndImpAsCSV(SOLUTION_PREFIX + "solutions_imp.csv", sol, impSol);
         }
     }
