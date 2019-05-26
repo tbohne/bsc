91584f0c758ceda05b6462ba7946f9fe733fad91
tbohne <tbohne@uos.de>
2019-05-26 20:57:46 +0200

    Swap - added doc / refined modifiers

--- StackingProblems/src/SP/post_optimization_methods/Swap.java
+++ StackingProblems/src/SP/post_optimization_methods/Swap.java
@@ -1,19 +1,41 @@
 package SP.post_optimization_methods;
 
+/**
+ * Represents a swap operation in the tabu search.
+ * A swap operation can be represented by two shift operations.
+ *
+ * @author Tim Bohne
+ */
 public class Swap {
 
-    Shift shiftOne;
-    Shift shiftTwo;
+    private final Shift shiftOne;
+    private final Shift shiftTwo;
 
+    /**
+     * Constructor
+     *
+     * @param shiftOne - first shift of the swap operation
+     * @param shiftTwo - second shift of the swap operation
+     */
     public Swap(Shift shiftOne, Shift shiftTwo) {
         this.shiftOne = shiftOne;
         this.shiftTwo = shiftTwo;
     }
 
+    /**
+     * Returns the swap's first shift operation.
+     *
+     * @return 1st shift operation
+     */
     public Shift getShiftOne() {
         return this.shiftOne;
     }
 
+    /**
+     * Returns the swap's second shift operation.
+     *
+     * @return 2nd shift operation
+     */
     public Shift getShiftTwo() {
         return this.shiftTwo;
     }
