db97f5416c154345852ff1f9e3381ff5dd6b3af1
tbohne <tbohne@uos.de>
2019-05-26 21:01:55 +0200

    Shift - refined methods / added doc

--- StackingProblems/src/SP/post_optimization_methods/Shift.java
+++ StackingProblems/src/SP/post_optimization_methods/Shift.java
@@ -2,32 +2,61 @@ package SP.post_optimization_methods;
 
 import SP.representations.StackPosition;
 
+/**
+ * Represents a shift operation in the tabu search.
+ *
+ * @author Tim Bohne
+ */
 public class Shift {
 
-    private int item;
-    private StackPosition shiftPos;
+    private final int item;
+    private final StackPosition shiftPos;
 
+    /**
+     * Constructor
+     *
+     * @param item     - item to be shifted
+     * @param shiftPos - position the item gets shifted to
+     */
     public Shift(int item, StackPosition shiftPos) {
         this.item = item;
         this.shiftPos = shiftPos;
     }
 
-    public int getItem() {
+    /**
+     * Returns the item to be shifted.
+     *
+     * @return item to be shifted
+     */
+    private int getItem() {
         return this.item;
     }
 
-    public StackPosition getShiftPos() {
+    /**
+     * Returns the position the item gets shifted to.
+     *
+     * @return position the item gets shifted to
+     */
+    private StackPosition getShiftPos() {
         return this.shiftPos;
     }
 
+    /**
+     * Enables equality check between shift operations.
+     *
+     * @param object - shift to be compared with
+     * @return whether or not the shift operations are equal
+     */
     @Override
     public boolean equals(Object object) {
-        if (object != null && object instanceof Shift) {
-            return (this.item == ((Shift)object).getItem() && this.shiftPos.equals(((Shift)object).getShiftPos()));
-        }
-        return false;
+        return object != null && object instanceof Shift && (this.item == ((Shift) object).getItem() && this.shiftPos.equals(((Shift) object).getShiftPos()));
     }
 
+    /**
+     * Returns the string representing the shift object.
+     *
+     * @return string representation of shift
+     */
     @Override
     public String toString() {
         return this.item + " --- " + this.shiftPos.toString();
